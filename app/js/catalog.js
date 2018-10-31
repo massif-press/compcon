c6f87e510afa317b5c52e2e48c9c11bd6b2c885f
Johnny <john.arena.3@gmail.com>
2018-10-30 23:23:29 -0500

    work on catalog, moved to dev branch

--- app/js/catalog.js
+++ app/js/catalog.js
@@ -6,9 +6,20 @@ const Expanders = require("./util/expander");
 const manufacturers = require("../resources/data/manufacturers.json");
 const core_bonuses = require("../resources/data/core_bonus.json");
 const pilot_gear = require("../resources/data/pilot_gear.json");
+const shells = require("../resources/data/shells.json");
 //templates
 const manufacturerTemplate = fs.readFileSync(__dirname + "/templates/catalog/manufacturers.hbs", "utf8");
 const bonusTemplate = fs.readFileSync(__dirname + "/templates/catalog/core-bonuses.hbs", "utf8");
+const shellTemplate = fs.readFileSync(__dirname + "/templates/catalog/shells.hbs", "utf8");
+
+Handlebars.registerHelper('haseString', function (stats) {
+  return "hase"
+});
+
+Handlebars.registerHelper('mountString', function (mounts) {
+  return "mounts"
+});
+  
 
 var template = Handlebars.compile(manufacturerTemplate);
 $("#manufacturer-sidebar").html(template({"manufacturers": manufacturers}));
@@ -23,18 +34,21 @@ $("#type-dropdown").change(function() {
 
   $(".subtype-dropdown").each(function(){$(this).hide(300)});
 
-  // var template;
+  var template;
   var itemObj = {};
   switch (val) {
     case "pilot_gear":
       // template = Handlebars.compile()
-      itemObj.items = pilot_gear;
-      $("#subtype-pg-dropdown").show(300);
-      break;
+      // itemObj.items = pilot_gear;
+      // $("#subtype-pg-dropdown").show(300);
+      // break;
     case "core_bonuses":
+      var template = Handlebars.compile(bonusTemplate);
       itemObj.items = core_bonuses;
       break;
     case "shells":
+       var template = Handlebars.compile(shellTemplate);
+      itemObj.items = shells;
       break;
     case "weapons":
       break;
@@ -45,7 +59,6 @@ $("#type-dropdown").change(function() {
   }
 
 
-  var template = Handlebars.compile(bonusTemplate);
   $("#scroll-body").html(template(itemObj));
   Expanders.bindEquipment();
 
