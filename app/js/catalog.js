efa31e00c4db00bf896ca45158369b43403578c3
Johnny <john.arena.3@gmail.com>
2018-10-31 23:18:26 -0500

    more catalog work

--- app/js/catalog.js
+++ app/js/catalog.js
@@ -7,10 +7,17 @@ const manufacturers = require("../resources/data/manufacturers.json");
 const core_bonuses = require("../resources/data/core_bonus.json");
 const pilot_gear = require("../resources/data/pilot_gear.json");
 const shells = require("../resources/data/shells.json");
+const weapons = require("../resources/data/weapons.json");
+const systems = require("../resources/data/systems.json");
 //templates
 const manufacturerTemplate = fs.readFileSync(__dirname + "/templates/catalog/manufacturers.hbs", "utf8");
+const pilotArmorTemplate = fs.readFileSync(__dirname + "/templates/catalog/pilot-armor.hbs", "utf8");
+const pilotWeaponTemplate = fs.readFileSync(__dirname + "/templates/catalog/pilot-weapons.hbs", "utf8");
+const pilotGearTemplate = fs.readFileSync(__dirname + "/templates/catalog/pilot-gear.hbs", "utf8");
 const bonusTemplate = fs.readFileSync(__dirname + "/templates/catalog/core-bonuses.hbs", "utf8");
 const shellTemplate = fs.readFileSync(__dirname + "/templates/catalog/shells.hbs", "utf8");
+const weaponTemplate = fs.readFileSync(__dirname + "/templates/catalog/weapons.hbs", "utf8");
+const systemTemplate = fs.readFileSync(__dirname + "/templates/catalog/systems.hbs", "utf8");
 
 Handlebars.registerHelper('haseString', function (stats, prop) {
   return stats[prop] ? stats[prop] : 0;
@@ -42,21 +49,50 @@ $("#type-dropdown").change(function() {
   var itemObj = {};
   switch (val) {
     case "pilot_gear":
-      // template = Handlebars.compile()
-      // itemObj.items = pilot_gear;
-      // $("#subtype-pg-dropdown").show(300);
-      // break;
+      $("#subtype-pg-dropdown").show(300);
+      break;
     case "core_bonuses":
       var template = Handlebars.compile(bonusTemplate);
       itemObj.items = core_bonuses;
       break;
     case "shells":
-       var template = Handlebars.compile(shellTemplate);
+      var template = Handlebars.compile(shellTemplate);
       itemObj.items = shells;
       break;
     case "weapons":
+      var template = Handlebars.compile(weaponTemplate);
+      itemObj.items = weapons;
       break;
     case "systems":
+      var template = Handlebars.compile(systemTemplate);
+      itemObj.items = systems;
+      break;
+    default:
+      break;
+  }
+
+  $("#scroll-body").html(template(itemObj));
+  tableInit()
+})
+
+//pilot gear type selector
+$("#subtype-pg-dropdown").change(function () {
+  var val = $(this).val();
+
+  var template;
+  var itemObj = {};
+  switch (val) {
+    case "armor":
+      var template = Handlebars.compile(pilotArmorTemplate)
+      itemObj.items = pilot_gear.armor;
+      break;
+    case "weapons":
+      var template = Handlebars.compile(pilotWeaponTemplate);
+      itemObj.items = pilot_gear.weapons;
+      break;
+    case "gear":
+      var template = Handlebars.compile(pilotGearTemplate);
+      itemObj.items = pilot_gear.gear;
       break;
     default:
       break;
