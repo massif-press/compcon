3d574d534cf182253630f8a9b1d15b05dfa7b468
Johnny <john.arena.3@gmail.com>
2018-11-15 19:54:20 -0600

    more level tool work

--- app/js/wizards/levelpilot.js
+++ app/js/wizards/levelpilot.js
@@ -6,11 +6,15 @@ const Licenses = require('../util/licensemanager');
 
 const w_talentTemplate = fs.readFileSync(__dirname + "/../templates/wizards/pilot-talent-upgrade.hbs", "utf8");
 const w_licenseTemplate = fs.readFileSync(__dirname + "/../templates/wizards/pilot-license-upgrade.hbs", "utf8");
+const w_coreskillsTemplate = fs.readFileSync(__dirname + "/../templates/wizards/pilot-coreskills.hbs", "utf8");
 
 const talents = require("../../resources/data/pilot_talents.json");
+const coreskills = require("../../resources/data/core_bonus.json");
 
 
 var selectedSkill;
+var selectedHASE;
+var core_skill_id;
 
 function init(pilot) {
 
@@ -29,6 +33,9 @@ function init(pilot) {
 
   //skills
   setSkillboxes(pilot);
+
+  //corebonuses
+  setCoreboxes(pilot);
 
   //talents 
   var selectableTalents = talents;
@@ -49,7 +56,6 @@ function init(pilot) {
   $("#talents-list").html(talTemp({ "talents": selectableTalents }));
 
   var talent_id = "";
-  
   $('.talent-upg').click(function () {
     var e = $(this);
     $(`.wizard-btn[data-step='3']`).removeClass('disabled').addClass('btn');
@@ -72,10 +78,10 @@ function init(pilot) {
 
   //Licenses
   for (var i = 0; i < licenseList.length; i++) {
-    var pIdx = pilot.licenses.findIndex(x => x.id === licenseList[i].id);
+    var pIdx = pilot.licenses.findIndex(x => x.name === licenseList[i].name);
 
     if (pIdx > -1) {
-      licenseList[i].pilot_rank = pilot.talents[pIdx].rank;
+      licenseList[i].pilot_rank = pilot.licenses[pIdx].rank;
     } else {
       licenseList[i].pilot_rank = 0;
     }
@@ -83,17 +89,36 @@ function init(pilot) {
 
   licenseList = licenseList.filter(l => l.pilot_rank < 3 && l.source !== "GMS");
 
+
   var licenseTemp = Handlebars.compile(w_licenseTemplate);
   $("#licenses-list").html(licenseTemp({ "licenses": licenseList }));
 
-  var license_id = "";
+  var newLicense;
+  $('.license-upg').click(function () {
+    var e = $(this);
+    if ((pilot.level + 1) % 3 === 0) $(`.wizard-btn[data-step='4']`).removeClass('disabled').addClass('btn');
+    else $(`.wizard-btn[data-step='5']`).removeClass('disabled').addClass('btn');
+    $('.license-upg').each(function () {
+      $(this).removeClass("skill-upgrade")
+      if ($(this).closest('.equip-expander').hasClass('open')) {
+        var parent = $(this).closest('.equip-expander');
+        $(this).removeClass('bold');
+        $(parent).removeClass('open');
+        $($(parent).find(".equip-open-info")).hide("swing");
+      }
+    })
+    e.addClass("skill-upgrade")
+    $("#selected-license").html(`Selected: <b>${e.data("name")} // ${e.data("level")}</b>`);
+    newLicense = {
+      name: e.data("name"),
+      source: e.data("source"),
+      level: e.data("level")
+    },
+    setCoreSkills(pilot, e.data("source"));
+  })
 
-
-
+  
   Expander.bindEquipment();
-
-
-
 
 
 }
@@ -118,7 +143,7 @@ function setSkillboxes(pilot) {
       e.off();
       e.click(function () {
         if (selectedSkill) setSkillboxes(pilot)
-        else(selectedSkill = skill.toLowerCase)
+        else(selectedSkill = skill.toLowerCase())
         $(`.wizard-btn[data-step='2']`).removeClass('disabled').addClass('btn');
         e.html(skill.toUpperCase());
         e.append(`<br><span class="subtitle skill-primary">${skillVal + 1 > 0 ? '+' : ''}${skillVal + 1}</span>`);
@@ -127,5 +152,79 @@ function setSkillboxes(pilot) {
     }
   });
 }
+
+function setCoreboxes(pilot) {
+  $(`.wizard-btn[data-step='2']`).addClass('disabled').removeClass('btn');
+
+  $('.core-skillbox').each(function () {
+    var e = $(this);   
+    var hase = e.data("hase");   
+    var haseVal = pilot.core[hase];
+    e.children('.subtitle').removeClass("skill-primary");
+    e.addClass('selectable').removeClass("skill-upgrade");
+    e.html(hase.toUpperCase());
+    var haseClass = haseVal > 0 ? 'skill-bonus' : 'skill-malus';
+    if (haseVal === 0) haseClass = '';
+    e.append(`<br><span class="hase-num ${haseClass}">${haseVal > 0 ? '+' : ''}${haseVal}</span>`)
+
+    if (haseVal > 6) {
+      e.addClass("disabled").removeClass("selectable");
+    } else {
+      e.off();
+      e.click(function () {
+        if (selectedHASE) setCoreboxes(pilot)
+        else(selectedHASE = hase.toLowerCase())
+        $(`.wizard-btn[data-step='3']`).removeClass('disabled').addClass('btn');
+        e.html(hase.toUpperCase());
+        e.append(`<br><span class="hase-num skill-primary">${haseVal + 1 > 0 ? '+' : ''}${haseVal + 1}</span>`);
+        e.addClass("skill-upgrade");
+      });
+    }
+  });
+}
+
+
+function setCoreSkills(pilot, addedLicenseSource) {
+  //TODO: refactor
+  //get array of license sources and levels
+  var larr = [];
+  for (let i = 0; i < pilot.licenses.length; i++) {
+    var l = pilot.licenses[i];
+    larr.push({
+      key: l.source,
+      val: l.level
+    })
+  }
+  larr.push({key: addedLicenseSource, val: 1});
+
+  //sum levels by source
+  var lls = {};
+  for (let i = 0; i < larr.length; i++) {
+    var l = larr[i];
+    if (lls[l.key]) lls[l.key] += l.val
+    else lls[l.key] = l.val;
+  }
+  //find all sources with level sums >= 3 (and GMS)
+  var availableSources = ["GMS"];
+  for (var l in lls) {
+    if (lls[l] >= 3) availableSources.push(l);
+  }
+
+  var selectableCBs = coreskills.filter(x => availableSources.includes(x.source));
+
+  var cbTemp = Handlebars.compile(w_coreskillTemplate);
+  $("#coreskills-list").html(cbTemp({"skills": selectableCBs}));
+
+  $('.coreskill-upg').click(function () {
+    var e = $(this);
+    $(`.wizard-btn[data-step='5']`).removeClass('disabled').addClass('btn');
+    $('.coreskill-upg').each(function () { $(this).removeClass("skill-upgrade");})
+    e.addClass("skill-upgrade")
+    $("#selected-coreskill").html(`Selected: <b>${e.data("name")}</b>`);
+    core_skill_id = e.data("id");
+  })
+
+}
+
 
 module.exports.init = init
