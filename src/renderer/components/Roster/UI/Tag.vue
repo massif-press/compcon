b84f1141291941d8150a82aa6b3d912ded2bba05
John Arena <john.arena@qsrsoft.com>
2019-02-19 15:26:28 -0600

    add tag chips

---
+++ src/renderer/components/Roster/UI/Tag.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <v-menu open-on-hover top offset-y>
+      <v-chip slot="activator" color="primary" dark>
+        <v-avatar>
+          <v-icon>label</v-icon>
+        </v-avatar>
+        {{ insert(tagData.name) }}
+      </v-chip>
+    <v-card>
+      <v-card-title>{{ insert(tagData.description) }}</v-card-title>
+    </v-card>
+    </v-menu>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'tag',
+    props: ['id', 'val'],
+    data: () => ({
+      tagData: {}
+    }),
+    methods: {
+      insert: function (str) {
+        return str.replace(/{VAL}/g, this.val)
+      }
+    },
+    mounted: function () {
+      this.tagData = this.$store.getters.getItemById('Tags', this.id)
+    }
+  }
+</script>
