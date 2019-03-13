cdee4d4ff860127c9ec87e2b5b1aa94725c57f90
Johnny <john.arena.3@gmail.com>
2019-03-13 16:25:18 -0500

    most of the mech loadout manager, various minor updates

--- src/renderer/components/Roster/UI/DamageElement.vue
+++ src/renderer/components/Roster/UI/DamageElement.vue
@@ -2,25 +2,25 @@
   <div style="display: inline-flex">
       <v-tooltip top v-if="small">
         <span slot="activator" :style="`font-size: ${size || 18}px; display: inline-flex;`">
-          <div v-for="n in dmg.length" :key="n + dmg[n-1].type + dmg[n-1].val + '_activator'">
-            <b v-if="dmg[n - 1].override"><v-icon>more_horiz</v-icon></b>
-            <b v-else :class="color(dmg[n - 1].type)">{{dmg[n - 1].val}} ¤</b>
-            <span v-if="dmg.length > n" class="grey--text">&nbsp;//&nbsp;</span>
+          <b v-if="dmg[0].override"><v-icon>more_horiz</v-icon></b>
+          <div v-else v-for="n in dmg.length" :key="n + dmg[n-1].type + dmg[n-1].val + '_activator'">
+            <b :class="color(dmg[n - 1].type)">{{dmg[n - 1].val}} ¤</b>
+            <span v-if="dmg.length > n" class="grey--text">//</span>
           </div>    
         </span>
         <span>
-          <div v-for="n in dmg.length" :key="n + dmg[n-1].type + dmg[n-1].val + '_tooltip'">
-            <b v-if="dmg[n - 1].override" class="text-capitalize"> {{dmg[n - 1].val}} </b>
-            <b v-else class="text-capitalize">{{dmg[n - 1].val}} {{dmg[n - 1].type}} damage</b>
+          <b v-if="dmg[0].override" class="text-capitalize"> {{dmg[0].val}} </b>
+          <div v-else v-for="n in dmg.length" :key="n + dmg[n-1].type + dmg[n-1].val + '_tooltip'">
+            <b class="text-capitalize">{{dmg[n - 1].val}} {{dmg[n - 1].type}} damage</b>
           </div>    
         </span>
       </v-tooltip>
 
       <span v-if="!small" :style="`font-size: ${size || 18}px`">
-        <div v-for="n in dmg.length" :key="n + dmg[n-1].type + dmg[n-1].val + '_damageEle'">
-          <b v-if="dmg[n - 1].override" class="text-capitalize"> {{dmg[n - 1].val}} </b>
-          <b v-else :class="color(dmg[n - 1].type)">{{dmg[n - 1].val}} {{dmg[n - 1].type}} damage</b>
-          <span v-if="dmg.length > n" class="grey--text">&nbsp;//&nbsp;</span>
+        <b v-if="dmg[0].override" class="text-capitalize"> {{dmg[0].val}} </b>
+        <div v-else v-for="n in dmg.length" :key="n + dmg[n-1].type + dmg[n-1].val + '_damageEle'">
+          <b :class="color(dmg[n - 1].type)">{{dmg[n - 1].val}} {{dmg[n - 1].type}} damage</b>
+          <span v-if="dmg.length > n" class="grey--text">//</span>
         </div>
       </span>
   </div>
@@ -32,25 +32,26 @@
     props: {
       dmg: Array,
       small: Boolean,
+      dark: Boolean,
       size: String
     },
     methods: {
       color: function (damageType) {
         switch (damageType.toLowerCase()) {
           case 'kinetic':
-            return 'grey--text text--darken-1  text-capitalize'
+            return this.dark ? 'grey--text text--lighten-1  text-capitalize' : 'grey--text text--darken-1  text-capitalize'
           case 'explosive':
-            return 'orange--text text--darken-4 text-capitalize'
+            return this.dark ? 'orange--text text--lighten-1 text-capitalize' : 'orange--text text--darken-4 text-capitalize'
           case 'energy':
-            return 'blue--text text--darken-3 text-capitalize'
+            return this.dark ? 'blue--text text--lighten-2 text-capitalize' : 'blue--text text--darken-3 text-capitalize'
           case 'variable':
-            return 'deep-purple--text text--darken-3 text-capitalize'
+            return this.dark ? 'purple--text text--lighten-2 text-capitalize' : 'deep-purple--text text--darken-3 text-capitalize'
           case 'heat':
-            return 'deep-orange--text text--lighten-4 text-capitalize'
+            return this.dark ? 'deep-orange--text text--lighten-1 text-capitalize' : 'deep-orange--text text--lighten-4 text-capitalize'
           case 'burn':
-            return 'red--text text--darken-1 text-capitalize'
+            return this.dark ? 'red--text text--lighten-1 text-capitalize' : 'red--text text--darken-1 text-capitalize'
           default:
-            return 'black text-capitalize'
+            return this.dark ? 'white text-capitalize' : 'black text-capitalize'
         }
       }
     }
---
+++ src/renderer/components/Roster/UI/WeaponCard.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="ma-1">
+    <v-card flat>
+      <v-card-text class="pb-0 pt-0">
+        <em v-if="!itemData.license">{{itemData.source}}<br></em>
+        <em v-else>{{itemData.source}} {{itemData.license}}, RANK <span v-for="n in itemData.license_level" :key="n">I</span><br></em>
+        <p v-if="itemData.description" v-html="itemData.description" class="description-text" />
+        <b>{{itemData.mount}} {{itemData.type}} <span v-if="itemData.sp">({{itemData.sp}} SP)</span></b>
+        <br>
+        <damage-element dark :dmg="itemData.damage" />
+        <range-element :range="itemData.range" />
+        <p v-if="itemData.effect" v-html="itemData.effect" class="pl-2"/>
+        <v-layout class="mt-2">
+          <tag v-for="(tag, index) in itemData.tags" :key="tag.id + index" :id="tag.id" :val="tag.val"/>
+        </v-layout>
+      </v-card-text>
+    </v-card>
+  </div>
+</template>
+
+<script>
+  import Tag from './Tag'
+  import DamageElement from './DamageElement'
+  import RangeElement from './RangeElement'
+
+  export default {
+    name: 'system-card',
+    props: ['itemData'],
+    components: { Tag, RangeElement, DamageElement }
+  }
+</script>
+
+<style>
+ .description-text {
+   font-weight: 300;
+   font-size: 15px;
+ }
+</style>
+
