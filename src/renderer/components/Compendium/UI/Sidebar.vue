4dd0b31827ff4f77bb1cf4fda98c144f60606ad1
Johnny <john.arena.3@gmail.com>
2019-03-24 15:55:47 -0500

    finish compendium item tables v1

--- src/renderer/components/Compendium/UI/Sidebar.vue
+++ src/renderer/components/Compendium/UI/Sidebar.vue
@@ -2,9 +2,9 @@
   <v-navigation-drawer :mini-variant.sync="mini" stateless touchless :value="isVisible" dark fixed class="pt-5" disable-route-watcher v-click-outside="minimize">
     <v-toolbar flat class="transparent pt-2">
       <v-list>
-        <v-list-tile @click="to('/licenses')">
+        <v-list-tile @click="to('licenses')" class="mb-2">
           <v-list-tile-action >
-            <v-icon>ballot</v-icon>
+            <v-icon>mdi-account-card-details</v-icon>
           </v-list-tile-action>
           <v-list-tile-content>
             <v-list-tile-title class="title">Licenses</v-list-tile-title>
@@ -13,45 +13,45 @@
 
         <v-divider />
 
-        <v-list-tile @click="to('licenses')">
+        <v-list-tile @click="to('manufacturers')">
           <v-list-tile-action>
-            <v-icon>group_work</v-icon>
+            <v-icon>mdi-factory</v-icon>
           </v-list-tile-action>
           <v-list-tile-content>
             <v-list-tile-title class="title">Manufacturers</v-list-tile-title>
           </v-list-tile-content>
         </v-list-tile>
 
-        <v-list-tile @click="to('licenses')">
+        <v-list-tile @click="to('frames')">
           <v-list-tile-action>
-            <v-icon>view_day</v-icon>
+            <v-icon>mdi-hexagon-outline</v-icon>
           </v-list-tile-action>
           <v-list-tile-content>
             <v-list-tile-title class="title">Frames</v-list-tile-title>
           </v-list-tile-content>
         </v-list-tile>
 
-        <v-list-tile @click="to('licenses')">
+        <v-list-tile @click="to('weapons')">
           <v-list-tile-action>
-            <v-icon>view_day</v-icon>
+            <v-icon>mdi-sword-cross</v-icon>
           </v-list-tile-action>
           <v-list-tile-content>
             <v-list-tile-title class="title">Weapons</v-list-tile-title>
           </v-list-tile-content>
         </v-list-tile>
 
-        <v-list-tile @click="to('licenses')">
+        <v-list-tile @click="to('systems')">
           <v-list-tile-action>
-            <v-icon>view_day</v-icon>
+            <v-icon>mdi-chip</v-icon>
           </v-list-tile-action>
           <v-list-tile-content>
             <v-list-tile-title class="title">Systems</v-list-tile-title>
           </v-list-tile-content>
         </v-list-tile>
 
-        <v-list-tile @click="to('licenses')">
+        <v-list-tile @click="to('pilot_gear')">
           <v-list-tile-action>
-            <v-icon>view_day</v-icon>
+            <v-icon>mdi-racing-helmet</v-icon>
           </v-list-tile-action>
           <v-list-tile-content>
             <v-list-tile-title class="title">Pilot Gear</v-list-tile-title>
