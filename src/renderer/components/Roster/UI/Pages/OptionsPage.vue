8ab6c16a53395c68503bc88d823fcd724e5f2561
John Arena <john.arena@qsrsoft.com>
2019-03-29 10:39:44 -0500

    fleshing out i/o

---
+++ src/renderer/components/Roster/UI/Pages/OptionsPage.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h2>Homebrew Content</h2>
+    <p>TODO: </p>
+    <v-btn block>Import Package</v-btn>
+    <hr>
+    <h3>Loaded Packages</h3>
+    <p>checkbox toggles go here</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'options-page'
+  }
+</script>
