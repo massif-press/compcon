870c0271c8311c9f422031dfbf7db57ad29a9f89
ari <sndaritsune@gmail.com>
2019-04-04 00:05:15 -0300

    ts some vue components

--- src/renderer/components/Roster/index.vue
+++ src/renderer/components/Roster/index.vue
@@ -1,77 +1,78 @@
 <template>
-  <div id='roster-index'>
-    <topbar />
-    
-    <div class="wrapper">
+    <div id="roster-index">
+        <topbar/>
 
-    <!-- Sidebar -->
-    <sidebar ref="sidebar" />
+        <div class="wrapper">
+            <!-- Sidebar -->
+            <sidebar ref="sidebar"/>
 
-    <!-- Page Content -->
-    <router-view class="pt-5"></router-view>
-
-    </div>    
-  </div>
+            <!-- Page Content -->
+            <router-view class="pt-5"></router-view>
+        </div>
+    </div>
 </template>
 
-<script>
-  import {Topbar, Sidebar} from './UI'
-  import PilotSheet from './PilotSheet/'
-  import ConfigSheet from './ConfigSheet/'
+<script lang="ts">
+import Vue from "vue";
+import { Topbar, Sidebar } from "./UI";
+import PilotSheet from "./PilotSheet/index.vue";
+import ConfigSheet from "./ConfigSheet/index.vue";
 
-  export default {
-    name: 'roster',
+export default Vue.extend({
+    name: "roster",
     components: { Topbar, Sidebar, PilotSheet, ConfigSheet },
     data: () => ({
-      activePilotId: ''
+        activePilotId: ""
     }),
     computed: {
-      pilots: function () {
-        return this.$store.getters.getAllPilots
-      }
+        pilots: function() {
+            return this.$store.getters.getAllPilots;
+        }
     },
-    created: function () {
-      this.$store.dispatch('setDatapath', this.userDataPath)
-      this.$store.dispatch('loadAllPilots')
-      this.$store.dispatch('loadData')
-      this.$store.dispatch('loadBrews')
-      this.$store.dispatch('buildLicenses')
+    created: function() {
+        this.$store.dispatch("setDatapath", this.$store.state.userDataPath);
+        this.$store.dispatch("loadAllPilots");
+        this.$store.dispatch("loadData");
+        this.$store.dispatch("loadBrews");
+        this.$store.dispatch("buildLicenses");
     },
     watch: {
-      $route (to, from) {
-        window.scrollTo(0, 0)
-        this.$refs.sidebar.isVisible = !(to.path === '/level' || to.path === '/new')
-      }
+        $route(to, from) {
+            window.scrollTo(0, 0);
+            this.$refs.sidebar.isVisible = !(
+                to.path === "/level" || to.path === "/new"
+            );
+        }
     }
-  }
+});
 </script>
 
 <style scoped>
-  @import url('https://fonts.googleapis.com/css?family=Source+Sans+Pro');
+@import url("https://fonts.googleapis.com/css?family=Source+Sans+Pro");
 
-  * {
+* {
     box-sizing: border-box;
     margin: 0;
     padding: 0;
-  }
+}
 
-  body { 
-    font-family: 'Source Sans Pro', sans-serif; 
+body {
+    font-family: "Source Sans Pro", sans-serif;
     overflow: hidden;
-  }
+}
 
-  .wrapper {
+.wrapper {
     display: flex;
     width: 100%;
 }
 </style>
 
 <style>
-  .roster-content {
+.roster-content {
     margin-left: 80px;
     margin-right: 0;
     width: 100vw;
     overflow-y: scroll;
-  }
+}
 </style>
 
