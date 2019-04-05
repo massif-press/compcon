606071ceaee6c28e065b4d15934bd82215ce9e63
John Arena <john.arena@qsrsoft.com>
2019-04-05 15:13:40 -0500

    roster/hangar views

---
+++ src/renderer/components/Roster/RosterView/PilotCard.vue
@@ -0,0 +1,200 @@
+<template>
+<v-hover>
+  <v-card slot-scope="{ hover }" :class="`${pilot.active ? 'active' : 'inactive'} elevation-${hover ? 12 : 0}`">
+    <v-layout row style="cursor: pointer;" @click="toPilotSheet()">
+      <v-flex v-if="pilot.portrait" class="ma-0 pb-0 pt-0">
+        <v-img :src="`file://${userDataPath}/img/portrait/${pilot.portrait}`" position="top" height="300px"/>
+      </v-flex>
+      <v-flex v-else class="ma-0 pb-0 pt-0 text-xs-center">
+        <div style="height: 300px; display:table; width:100%">
+          <span class="pilot-letter white--text">{{pilot.callsign.substring(0, 1).toUpperCase()}}</span>
+        </div>
+      </v-flex>
+    </v-layout>
+    <v-layout row>
+      <v-flex>
+        <v-card color="rgba(0, 0, 0, .55)" dark flat>
+          <v-layout>
+            <v-flex class="ma-2">
+              <span class="title">{{pilot.callsign}}</span>
+              <br>
+              <span class="caption">{{pilot.name}}<br>{{background(pilot)}}, LL{{pilot.level}}</span>
+            </v-flex>
+            <v-flex xs4 class="ma-2">
+              <v-tooltip top>
+                <v-btn slot="activator" icon class="ma-0" disabled @click="toggleActive()"><v-icon>mdi-power</v-icon></v-btn>
+                <span>Activate Pilot<br>(feature in development)</span>
+              </v-tooltip>
+              <v-tooltip top>
+                <v-btn slot="activator" icon class="ma-0" @click="copyDialog = true"><v-icon>mdi-content-duplicate</v-icon></v-btn>
+                <span>Clone Pilot</span>
+              </v-tooltip>
+              <v-tooltip top>
+                <v-btn slot="activator" icon class="ma-0" @click="exportDialog = true"><v-icon>mdi-export-variant</v-icon></v-btn>
+                <span>Export Pilot</span>
+              </v-tooltip>
+              <v-tooltip top>
+              <v-btn slot="activator" icon class="ma-0" @click="deleteDialog = true"><v-icon>delete</v-icon></v-btn>
+                <span>Delete Pilot</span>
+              </v-tooltip>
+            </v-flex>
+          </v-layout>
+        </v-card>
+      </v-flex>
+
+      <v-snackbar v-model="snackbar" :timeout="5000" >
+        <span v-html="notification" />
+        <v-btn color="pink" flat @click="snackbar = false" > Close </v-btn>
+      </v-snackbar>
+
+      <v-dialog v-model="deleteDialog" width="500" >
+        <v-card>
+          <v-card-text>
+            Are you sure you want to delete {{pilot.callsign}}? This action cannot be undone
+          </v-card-text>
+          <v-divider />
+          <v-card-actions>
+            <v-btn color="primary" flat @click="deleteDialog = false"> Cancel </v-btn>
+            <v-spacer></v-spacer>
+            <v-btn color="error" @click="deletePilot"> Delete Pilot </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+
+      <v-dialog v-model="exportDialog" width="500" >
+        <v-card>
+          <v-card-title class="title">Export Pilot &mdash; {{pilot.callsign}}</v-card-title>
+          <v-card-text>
+            <v-btn large block flat color="primary" @click="exportPilot">Save to File</v-btn>
+            <br>
+            <v-btn large block flat color="primary" @click="copyPilot">Copy Pilot Data to Clipboard</v-btn>
+          </v-card-text>
+          <v-divider />
+          <v-card-actions>
+            <v-btn color="primary" flat @click="exportDialog = false"> Cancel </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+
+      <v-dialog v-model="copyDialog" width="650" >
+        <v-card>
+          <v-card-title class="title">Clone Pilot &mdash; {{pilot.callsign}}</v-card-title>
+          <v-card-text class="text-xs-center">
+              <v-btn large block color="indigo" @click="clonePilot()" dark>Duplicate Pilot</v-btn>
+              <span>Copy Pilot data into a new Pilot, without generating a <b>Quirk</b></span>
+            <v-divider class="ma-4"/>
+              <v-btn large block color="indigo" dark @click="clonePilot(true)">Flash Clone Pilot</v-btn>
+              <span>Flash Clones this Pilot, and generates a <b>Quirk</b> &mdash; a complication resulting from the cloning process
+              <br>
+              <i>(Quirks can be edited or removed on the Pilot Sheet)</i></span>
+          </v-card-text>
+          <v-divider />
+          <v-card-actions>
+            <v-btn color="primary" flat @click="copyDialog = false"> Cancel </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+
+    </v-layout>
+  </v-card>
+</v-hover>
+</template>
+
+<script>
+import io from '@/store/data_io'
+
+export default {
+  name: 'pilot-card',
+  props: {
+    pilot: Object,
+    pIdx: Number
+  },
+  data: () => ({
+    deleteDialog: false,
+    exportDialog: false,
+    copyDialog: false,
+    notification: '',
+    snackbar: false
+  }),
+  methods: {
+    background: function () {
+      if (this.pilot.custom_background) return this.pilot.custom_background
+      else return this.$store.getters.getItemById('Backgrounds', this.pilot.background).name
+    },
+    toggleActive: function () {
+      this.$store.dispatch('loadPilot', this.pilot.id)
+      this.$store.dispatch('editPilot', {
+        attr: `active`,
+        val: !this.pilot.active
+      })
+    },
+    toPilotSheet: function () {
+      this.$store.dispatch('loadPilot', this.pilot.id)
+      this.$router.push('./pilot')
+    },
+    deletePilot: function () {
+      this.deleteDialog = false
+      this.$store.dispatch('deletePilot', this.pilot.id)
+      this.notification = 'Pilot Deleted'
+      this.snackbar = true
+    },
+    clonePilot: function (isFlashclone) {
+      if (isFlashclone) {
+        var quirks = this.$store.getters.getItemCollection('Quirks')
+        var quirk = quirks[Math.floor(Math.random() * quirks.length)]
+        console.log(quirk)
+        this.$store.dispatch('clonePilot', {id: this.pilot.id, quirk: quirk})
+        this.notification = 'Pilot Cloned'
+      } else {
+        this.$store.dispatch('clonePilot', {id: this.pilot.id})
+        this.notification = 'Pilot Duplicated'
+      }
+      this.snackbar = true
+      this.copyDialog = false
+    },
+    exportPilot: function () {
+      const { dialog } = require('electron').remote
+      var path = dialog.showSaveDialog({
+        defaultPath: this.pilot.callsign.toLowerCase().replace(/\W/g, ''),
+        buttonLabel: 'Save Pilot'
+      })
+      io.saveFile(path + '.json', JSON.stringify(this.pilot), function (err) {
+        if (err) {
+          alert(`Error: COMP/CON could not save a file to ${path}`)
+        } else {
+          this.exportDialog = false
+          this.notification = 'Pilot Export Successful'
+          this.snackbar = true
+        }
+      })
+    },
+    copyPilot: function () {
+      const {clipboard} = require('electron')
+      clipboard.writeText(JSON.stringify(this.pilot))
+      this.exportDialog = false
+      this.notification = 'Pilot Data Copied to Clipboard'
+      this.snackbar = true
+    }
+  }
+}
+</script>
+
+<style>
+ .pilot-letter {
+   display:table-cell; 
+   vertical-align:middle; 
+   text-align:center;
+   font-size: 20vw;
+   line-height: 0;
+ }
+
+ .active {
+   background: linear-gradient(#283593, #424242 80%);
+   background-color: #424242;
+ }
+
+  .inactive {
+   background: linear-gradient(#616161, #424242 80%);
+   background-color: #424242;
+ }
+</style>
---
+++ src/renderer/components/Roster/RosterView/index.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="roster-view">
+    <v-container fluid class="pa-0 pt-3">
+      <v-layout><v-flex class="text-xs-center roster-title"><span>pilot roster</span></v-flex></v-layout>
+    </v-container>
+    <v-container grid-list-xl fluid>
+      <v-layout row wrap class="ml-2 mr-2" fill-height>
+        <v-flex v-for="(p, i) in pilots" :key="p.id" class="mb-4" xs3>
+          <pilot-card :pilot="p" :p-idx="i"/>
+        </v-flex>
+        <v-flex xs3><add-pilot-card /></v-flex>
+      </v-layout>
+    </v-container>
+  </div>
+</template>
+
+<script>
+import PilotCard from './PilotCard'
+import AddPilotCard from './AddPilotCard'
+
+export default {
+  name: 'roster-view',
+  components: { PilotCard, AddPilotCard },
+  computed: {
+    pilots: function () {
+      return this.$store.getters.getAllPilots
+    }
+  }
+}
+</script>
+
+<style>
+  .roster-view {
+    width: 100%;
+    height: 100vh;
+    overflow-y: scroll;
+    background-color: #424242;
+  }
+
+  .roster-title {
+    font-size: 35px;
+    letter-spacing: 25px;
+    font-weight: 100;
+    text-transform: uppercase;
+    color: #fff;
+  }
+</style>
