379bb3c9772030e13e30b5c2ed69d3720a7f76dc
ari <sndaritsune@gmail.com>
2019-04-08 19:02:05 -0300

    fix directive for typescript

--- src/renderer/main.ts
+++ src/renderer/main.ts
@@ -21,17 +21,17 @@ Vue.use(Vuetify, {
 })
 
 Vue.directive('click-outside', {
-  bind: function (el, binding, vnode) {
-    el.clickOutsideEvent = function (event) {
+  bind: (el: any, binding: any, vnode: any) => {
+    el.clickOutsideEvent = (event: Event) => {
       if (!(el === event.target || el.contains(event.target))) {
         vnode.context[binding.expression](event)
       }
     }
     document.body.addEventListener('click', el.clickOutsideEvent)
   },
-  unbind: function (el) {
+  unbind: (el: any) => {
     document.body.removeEventListener('click', el.clickOutsideEvent)
-  }
+  },
 })
 
 /* eslint-disable no-new */
