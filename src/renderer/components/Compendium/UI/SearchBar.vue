3afe880ad54948d26e9537a6e8a9e0175342d906
ari <sndaritsune@gmail.com>
2019-04-10 22:31:02 -0300

    ?????

--- src/renderer/components/Compendium/UI/SearchBar.vue
+++ src/renderer/components/Compendium/UI/SearchBar.vue
@@ -1,5 +1,4 @@
 <template>
-    <v-toolbar :manual-scroll="!active" class="tbar" ref="main">
       <v-text-field
         ref="input"
         class="search-field"
@@ -8,9 +7,9 @@
         solo
         hide-details
         single-line
-        @blur="active = false"
+        placeholder="Search the Compendium"
+        @keyup.enter="doSearch"
       />
-    </v-toolbar>
 </template>
 
 <script lang="ts">
@@ -20,32 +19,26 @@ import { debounce } from 'lodash'
 export default Vue.extend({
     name: 'compendium-search-bar',
     data: () => ({
-      active: false,
       searchText: '',
+      eventListener: null,
     }),
+    methods: {
+      doSearch() {
+        this.$router.push(`/compendiumsearch/${this.searchText}`)
+      },
+    },
     mounted() {
-      window.addEventListener('keydown',
-        debounce((e) => {
-          if (!this.active) {
-            if (e.keyCode === 70 && e.ctrlKey) {
-              this.active = true
-              const input = this.$refs.input as HTMLInputElement
-              input.focus()
-            }
-          }
-        }),
-      )
+      // window.addEventListener('keydown',
+      //   debounce((e) => {
+      //     if (!this.active) {
+      //       if (e.keyCode === 70 && e.ctrlKey) {
+      //         this.active = true
+      //         const input = this.$refs.input as HTMLInputElement
+      //         input.focus()
+      //       }
+      //     }
+      //   }),
+      // )
     },
   })
 </script>
-
-<style scoped>
-.tbar {
-  width: 96vw;
-  position: fixed;
-}
-.search-field {
-  width: 96vw;
-  margin-left: auto;
-}
-</style>
---
+++ src/renderer/components/Compendium/Views/Home.vue
@@ -0,0 +1,137 @@
+<template>
+  <v-container fluid justify-center>
+    <v-layout row justify-center class="compendium-header" mt-5 mb-4>
+        <h1 class="display-1 font-weight-thin">Compendium</h1>
+    </v-layout>
+    <v-layout row wrap>
+        <v-flex xs12 px-2 pb-3>
+            <search-bar />
+        </v-flex>
+        <v-flex xs6 px-2>
+            <v-btn
+             to="licenses"
+             outline
+             flat
+             block
+             color="green"
+             text-uppercase
+            >
+                licenses
+            </v-btn>
+        </v-flex>
+        <v-flex xs6 px-2>
+            <v-btn
+             to="manufacturers"
+             outline
+             flat
+             block
+             color="green"
+             text-uppercase
+            >
+                manufacturers
+            </v-btn>
+        </v-flex>
+        <v-flex xs3 px-2>
+            <v-btn
+             to="frames"
+             large
+             outline
+             flat
+             block
+             color="purple"
+             text-uppercase
+            >
+                frames
+            </v-btn>
+        </v-flex>
+        <v-flex xs3 px-2>
+            <v-btn
+             to="weapons"
+             large
+             outline
+             flat
+             block
+             color="pink darken-2"
+             text-uppercase
+            >
+                weapons
+            </v-btn>
+        </v-flex>
+        <v-flex xs3 px-2>
+            <v-btn
+             to="systems"
+             large
+             outline
+             flat
+             block
+             color="teal"
+             text-uppercase
+            >
+                systems
+            </v-btn>
+        </v-flex>
+        <v-flex xs3 px-2>
+            <v-btn
+             to="pilot_gear"
+             large
+             outline
+             flat
+             block
+             color="primary"
+             text-uppercase
+            >
+                pilot gear
+            </v-btn>
+        </v-flex>
+        <v-flex xs6 px-2>
+            <v-btn
+             to="statuses"
+             outline small
+             flat
+             block
+             color="secondary"
+             text-uppercase
+            >
+                statuses
+            </v-btn>
+        </v-flex>
+        <v-flex xs6 px-2>
+            <v-btn
+             to="tags"
+             outline small
+             flat
+             block
+             color="secondary"
+             text-uppercase
+            >
+                tags
+            </v-btn>
+        </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { SearchBar } from '../UI'
+
+export default Vue.extend({
+    name: 'compendium-home',
+    components: { SearchBar },
+})
+</script>
+
+<style scoped>
+.container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-top: 5em;
+}
+.compendium-header h1 {
+    font-size: 3em !important;
+    letter-spacing: 0.2em !important;
+    text-align: center;
+    text-transform: uppercase;
+}
+</style>
