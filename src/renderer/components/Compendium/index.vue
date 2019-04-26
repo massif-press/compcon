0fa2a79e7255a6a37081650debfa90c7eceef3a6
ari <sndaritsune@gmail.com>
2019-04-25 23:19:35 -0300

    remove sidebar

--- src/renderer/components/Compendium/index.vue
+++ src/renderer/components/Compendium/index.vue
@@ -1,7 +1,5 @@
 <template>
   <div>
-    <!-- <sidebar /> -->
-      <!-- <router-view style="margin-left: 80px; width:96vw"></router-view> -->
       <v-slide-y-reverse-transition mode="out-in">
         <router-view style="width: 100vw;" />
       </v-slide-y-reverse-transition>
@@ -10,10 +8,8 @@
 
 <script lang="ts">
 import Vue from 'vue'
-import Sidebar from './Sidebar.vue'
 
 export default Vue.extend({
     name: 'compendium',
-    components: { Sidebar },
   })
 </script>
