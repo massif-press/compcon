21265d1f16a602ef36f5fe452c0f61b7ce8468b1
Scott Macmillan <CommanderTso@users.noreply.github.com>
2019-06-10 16:14:42 -0400

    Rewrite CI to run tests

---
+++ build_scripts/install_aws.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
+unzip awscli-bundle.zip
+sudo ./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
