2bc525ffb5e70dd8a29f163cbe3f0e18f7271fc2
Scott Macmillan <CommanderTso@users.noreply.github.com>
2019-06-19 07:20:28 -0400

    Add unit tests for DiceRoller._rollDie()

--- src/renderer/features/_shared/classes/DiceRoller.unit.test.ts
+++ src/renderer/features/_shared/classes/DiceRoller.unit.test.ts
@@ -2,12 +2,17 @@
 import { DiceRoller } from '@/class'
 import 'jest'
 
-beforeEach(() => {})
+// beforeEach(() => {})
 
-afterEach(() => {})
+// afterEach(() => {})
 
-test('_rollDie', () => {
-  Math.random = () => 0.5
-
-  expect(DiceRoller._rollDie(6)).toBe(4)
+test('_rollDie outputs are correct', () => {
+  Math.random = () => 0.0
+  expect(DiceRoller._rollDie(10)).toBe(1)
+  Math.random = () => 0.0999
+  expect(DiceRoller._rollDie(10)).toBe(1)
+  Math.random = () => 0.4500
+  expect(DiceRoller._rollDie(10)).toBe(5)
+  Math.random = () => 0.9999
+  expect(DiceRoller._rollDie(10)).toBe(10)
 })
