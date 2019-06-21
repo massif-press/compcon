4daac178fce182f3bd8836fd5df54265af8d52bd
Scott Macmillan <CommanderTso@users.noreply.github.com>
2019-06-21 08:16:26 -0400

    Start on unit tests for DiceRoller.rollSkillCheck()

--- src/renderer/features/_shared/classes/DiceRoller.unit.test.ts
+++ src/renderer/features/_shared/classes/DiceRoller.unit.test.ts
@@ -1,4 +1,6 @@
-import { DiceRoller } from '@/class'
+import { 
+  DiceRoller,
+  SkillRollResult } from '@/class'
 import 'jest'
 import { mockRandom, resetMockRandom } from 'jest-mock-random'
 
@@ -6,22 +8,59 @@ afterEach(() => {
   resetMockRandom()
 })
 
-describe('_rollDie', () => {
-  it('outputs are correct', () => {
-    mockRandom([0.0, 0.0999, 0.4500, 0.9999])
+describe('rollSkillCheck', () => {
+  it('sets optional args to zero if not passed', () => {
+    mockRandom([.499])
 
-    expect(DiceRoller._rollDie(10)).toBe(1)
-    expect(DiceRoller._rollDie(10)).toBe(1)
-    expect(DiceRoller._rollDie(10)).toBe(5)
-    expect(DiceRoller._rollDie(10)).toBe(10)
+    let result = DiceRoller.rollSkillCheck()
+    expect(result).toBeInstanceOf(SkillRollResult)
+    expect(result.total).toBe(10)
+    expect(result.staticBonus).toBe(0)
+    expect(result.total).toBe(10)
+  })
+})
+
+describe('_rollAccuracyDice', () => {
+  it('returns correct results for positive dice', () => {
+    mockRandom([.399, .678, .555, .444, .999])
+    
+    expect(DiceRoller._rollAccuracyDice(5)).toEqual(
+      expect.objectContaining({
+        result: 6,
+        rolls: [3, 5, 4, 3, 6]
+      })
+    )
   })
 
-  it('returns 0 for negative die type', () => {
-    expect(DiceRoller._rollDie(-5)).toBe(0)
+  it('returns correct results when dice roll identically', () => {
+    mockRandom([.399])
+
+    expect(DiceRoller._rollAccuracyDice(6)).toEqual(
+      expect.objectContaining({
+        result: 3,
+        rolls: [3, 3, 3, 3, 3, 3]
+      })
+    )
   })
 
-  it('returns 0 for 0 die type', () => {
-    expect(DiceRoller._rollDie(0)).toBe(0)
+  it('returns correct results for negative dice', () => {
+    mockRandom([.399, .678, .555, .444, .999])
+
+    expect(DiceRoller._rollAccuracyDice(-5)).toEqual(
+      expect.objectContaining({
+        result: -6,
+        rolls: [3, 5, 4, 3, 6]
+      })
+    )
+  })
+
+  it('returns 0 & [] for zero dice', () => {
+    expect(DiceRoller._rollAccuracyDice(0)).toEqual(
+      expect.objectContaining({
+        result: 0,
+        rolls: []
+      })
+    )
   })
 })
 
@@ -83,46 +122,21 @@ describe('_rollDieSet', () => {
   })
 })
 
-describe('_rollAccuracyDice', () => {
-  it('returns correct results for positive dice', () => {
-    mockRandom([.399, .678, .555, .444, .999])
-    
-    expect(DiceRoller._rollAccuracyDice(5)).toEqual(
-      expect.objectContaining({
-        result: 6,
-        rolls: [3, 5, 4, 3, 6]
-      })
-    )
+describe('_rollDie', () => {
+  it('outputs are correct', () => {
+    mockRandom([0.0, 0.0999, 0.4500, 0.9999])
+
+    expect(DiceRoller._rollDie(10)).toBe(1)
+    expect(DiceRoller._rollDie(10)).toBe(1)
+    expect(DiceRoller._rollDie(10)).toBe(5)
+    expect(DiceRoller._rollDie(10)).toBe(10)
   })
 
-  it('returns correct results when dice roll identically', () => {
-    mockRandom([.399])
-
-    expect(DiceRoller._rollAccuracyDice(6)).toEqual(
-      expect.objectContaining({
-        result: 3,
-        rolls: [3, 3, 3, 3, 3, 3]
-      })
-    )
+  it('returns 0 for negative die type', () => {
+    expect(DiceRoller._rollDie(-5)).toBe(0)
   })
 
-  it('returns correct results for negative dice', () => {
-    mockRandom([.399, .678, .555, .444, .999])
-
-    expect(DiceRoller._rollAccuracyDice(-5)).toEqual(
-      expect.objectContaining({
-        result: -6,
-        rolls: [3, 5, 4, 3, 6]
-      })
-    )
-  })
-
-  it('returns 0 & [] for zero dice', () => {
-    expect(DiceRoller._rollAccuracyDice(0)).toEqual(
-      expect.objectContaining({
-        result: 0,
-        rolls: []
-      })
-    )
+  it('returns 0 for 0 die type', () => {
+    expect(DiceRoller._rollDie(0)).toBe(0)
   })
 })
