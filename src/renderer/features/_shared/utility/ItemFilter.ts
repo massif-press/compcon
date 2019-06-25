7cf7ddbbc98828e0b0d8296a37169919011409ce
John Arena <john.arena@qsrsoft.com>
2019-06-25 10:16:20 -0500

    first pass mech equip filters

---
+++ src/renderer/features/_shared/utility/ItemFilter.ts
@@ -0,0 +1,91 @@
+import {
+  MechWeapon,
+  SystemType,
+  WeaponType,
+  WeaponSize,
+  RangeType,
+  DamageType,
+  MechEquipment,
+} from '@/class'
+import MechSystem from '../classes/MechSystem'
+
+declare interface IEquipmentFilter {
+  license: string[]
+  include: string[]
+  exclude: string[]
+  source: string[]
+}
+
+declare interface IWeaponFilter extends IEquipmentFilter {
+  weaponType: WeaponType[]
+  weaponSize: WeaponSize[]
+  attackType: RangeType[]
+  damageType: DamageType[]
+}
+
+declare interface ISystemFilter extends IEquipmentFilter {
+  systemType: SystemType[]
+}
+
+class ItemFilter {
+  public static FilterEquipment(
+    items: MechEquipment[],
+    filter: IEquipmentFilter
+  ): MechEquipment[] {
+    if (filter.license && filter.license.length) {
+      items = items.filter(x =>
+        filter.license.includes(x.License.toLowerCase())
+      )
+    }
+    if (filter.include && filter.include.length) {
+      items = items.filter(x => x.Tags.some(t => filter.include.includes(t.ID)))
+    }
+    if (filter.exclude && filter.exclude.length) {
+      items = items.filter(
+        x => !x.Tags.some(t => filter.include.includes(t.ID))
+      )
+    }
+    if (filter.source && filter.source.length) {
+      items = items.filter(x => filter.source.includes(x.Source))
+    }
+    return items
+  }
+
+  public static FilterWeapons(
+    items: MechWeapon[],
+    filter: IWeaponFilter
+  ): MechWeapon[] {
+    items = this.FilterEquipment(items, filter) as MechWeapon[]
+    if (filter.weaponType && filter.weaponType.length) {
+      items = items.filter(x => filter.weaponType.includes(x.Type))
+    }
+    if (filter.weaponSize && filter.weaponSize.length) {
+      items = items.filter(x => filter.weaponSize.includes(x.Size))
+    }
+    if (filter.attackType && filter.attackType.length) {
+      items = items.filter(x =>
+        x.Range.some(r => filter.attackType.includes(r.Type))
+      )
+    }
+    if (filter.damageType && filter.damageType.length) {
+      items = items.filter(x =>
+        x.Damage.some(d => filter.damageType.includes(d.Type))
+      )
+    }
+
+    return items
+  }
+
+  public static FilterSystems(
+    items: MechSystem[],
+    filter: ISystemFilter
+  ): MechSystem[] {
+    items = this.FilterEquipment(items, filter) as MechSystem[]
+    if (filter.systemType && filter.systemType.length) {
+      items = items.filter(x => filter.systemType.includes(x.Type))
+    }
+    return items
+  }
+}
+
+export default ItemFilter
