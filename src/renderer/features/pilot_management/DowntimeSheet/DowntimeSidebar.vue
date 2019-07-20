094d9184e853ccf2d9c3c63328037ed3afe56125
John Arena <john.arena@qsrsoft.com>
2019-07-20 14:24:24 -0500

    wip

---
+++ src/renderer/features/pilot_management/DowntimeSheet/DowntimeSidebar.vue
@@ -0,0 +1,85 @@
+<template>
+  <v-navigation-drawer
+    v-model="drawer"
+    :mini-variant="mini"
+    absolute
+    dark
+    right
+    mini-variant-width="40"
+    width="550px"
+    class="elevation-10"
+    style="padding-top: 60px; z-index: 9"
+  >
+    <div class="panel-expander">
+      <v-layout column fill-height justify-center>
+        <v-flex xs6>
+          <v-divider dark vertical class="ml-4" />
+        </v-flex>
+        <v-flex shrink>
+          <v-btn flat icon dark @click.stop="mini = !mini">
+            <v-icon large v-if="mini">mdi-chevron-double-left</v-icon>
+            <v-icon large v-else>mdi-chevron-double-right</v-icon>
+          </v-btn>
+        </v-flex>
+        <v-flex xs6>
+          <v-divider dark vertical class="ml-4" />
+        </v-flex>
+      </v-layout>
+    </div>
+    <v-fade-transition>
+      <v-window
+        v-show="!mini"
+        v-model="step"
+        class="ml-5 white--text"
+        style="height:100%; width: 85%"
+      >
+        <v-window-item :value="0">
+          <v-layout column fill-height>
+            <v-flex xs12 class="major-title text-xs-center">
+              <span>DOWNTIME</span>
+            </v-flex>
+          </v-layout>
+          <v-divider class="ma-2" dark />
+          <div class="ma-2 pt-3">
+            <action-button action-id="action_poweratcost" class="mb-1" />
+            <action-button action-id="action_buysometime" class="mb-1" />
+            <action-button action-id="action_damndrink" class="mb-1" />
+            <action-button action-id="action_creative" class="mb-1" />
+            <action-button action-id="action_focused" class="mb-1" />
+            <action-button action-id="action_organized" class="mb-1" />
+            <action-button action-id="action_information" class="mb-1" />
+            <action-button action-id="action_connected" class="mb-1" />
+            <action-button action-id="action_scrounge" class="mb-1" />
+          </div>
+        </v-window-item>
+      </v-window>
+    </v-fade-transition>
+  </v-navigation-drawer>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import ActionButton from '../ActiveSheet/components/Turn/ActionButton.vue'
+
+export default Vue.extend({
+  name: 'downtime-sidebar',
+  components: { ActionButton },
+  props: {
+    pilot: Object,
+    mech: Object,
+    loadout: Object,
+  },
+  data: () => ({
+    step: 0,
+    drawer: true,
+    mini: true,
+  }),
+})
+</script>
+
+<style scoped>
+.panel-expander {
+  position: fixed;
+  height: 100%;
+}
+</style>
---
+++ src/renderer/features/pilot_management/DowntimeSheet/components/PilotSkillCard.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-flex xs4>
+    <div class="ma-1">
+      <v-toolbar dark dense style="background-color: #616161">
+        <v-chip small dark color="primary">
+          <v-avatar class="mr-0">
+            <v-icon>add</v-icon>
+          </v-avatar>
+          <b class="font-weight-bold" style="font-size: 16px">{{pSkill.Bonus}}</b>
+        </v-chip>
+        <v-toolbar-title>{{ pSkill.Skill.Name }}</v-toolbar-title>
+
+        <v-spacer />
+
+        <v-toolbar-items>
+          <v-dialog width="50vw">
+            <template v-slot:activator="{ on }">
+              <v-btn absolute right style="right: 0; top: 0" icon flat dark v-on="on">
+                <v-icon dark>mdi-information-outline</v-icon>
+              </v-btn>
+            </template>
+            <v-card>
+              <v-card-title class="major-title primary white--text">{{ pSkill.Skill.Name }}</v-card-title>
+              <v-card-text>
+                <p class="effect-text" v-html="pSkill.Skill.Description" />
+                <p class="fluff-text" v-html="pSkill.Skill.Detail" />
+              </v-card-text>
+            </v-card>
+          </v-dialog>
+        </v-toolbar-items>
+      </v-toolbar>
+    </div>
+  </v-flex>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'pilot-skill-card',
+  props: {
+    pSkill: Object,
+  },
+})
+</script>
