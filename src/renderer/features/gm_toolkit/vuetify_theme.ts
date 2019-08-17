f8b642c6aae5abff081396e3934310504031fe9b
Eranziel <eranziel@gmail.com>
2019-08-17 15:53:04 -0600

    Add role and color for Biological NPCs.

--- src/renderer/features/gm_toolkit/vuetify_theme.ts
+++ src/renderer/features/gm_toolkit/vuetify_theme.ts
@@ -4,6 +4,7 @@ export default {
   'role--controller': '#398ad6',
   'role--support': '#6aa84f',
   'role--artillery': '#a64d79',
+  'role--biological': '#7e52a3',
 
   'system--weapon': '#cc0000',
   'system--system': '#2aa780',
