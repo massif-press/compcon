da30babead7a8c8bf37383b0d0787f7c59006020
John Arena <john.arena@qsrsoft.com>
2019-08-29 15:09:27 -0500

    pilot roster

--- src/renderer/features/pilot_management/Roster/index.vue
+++ src/renderer/features/pilot_management/Roster/index.vue
@@ -1,80 +1,54 @@
 <template>
-  <div>
-    <h1 class="heading margin-nav">Pilot Roster</h1>
+  <v-container fluid>
+    <h1 class="heading margin-nav pl-2">Pilot Roster</h1>
 
-    <v-menu offset-y open-on-hover>
-      <template v-slot:activator="{ on }">
-        <v-btn icon absolute top right class="margin-nav-absolute" v-on="on">
-          <v-icon>mdi-sort</v-icon>
-        </v-btn>
-      </template>
-      <v-list dense>
-        <v-list-item v-for="s in sorts" :key="s.name">
-          <v-list-item-title>{{ s.name }}</v-list-item-title>
-          <v-spacer class="ml-3" />
-          <v-btn icon class="pa-0 ma-0" @click="sortBy(s, true)">
-            <v-icon color="primary">mdi-sort-ascending</v-icon>
-          </v-btn>
-          <v-btn icon class="pa-0 ma-0" @click="sortBy(s, false)">
-            <v-icon color="primary">mdi-sort-descending</v-icon>
-          </v-btn>
-        </v-list-item>
-      </v-list>
-    </v-menu>
+    <v-btn-toggle v-model="listOption" mandatory>
+      <v-btn small icon>
+        <v-icon color="primary">mdi-view-list</v-icon>
+      </v-btn>
+      <v-btn small icon>
+        <v-icon color="primary">mdi-format-align-justify</v-icon>
+      </v-btn>
+    </v-btn-toggle>
+    <roster-sort :pilots="pilots" @sort="sortPilots" />
 
-    <v-container grid-list-xl fluid>
-      <v-row wrap class="ml-2 mr-2 mb-5" fill-height>
-        <v-col v-for="(p, i) in pilots" :key="p.id" class="mb-4" xs3>
-          <pilot-card :pilot="p" :p-idx="i" :card-height="cardHeight" />
-        </v-col>
-        <v-col xs3 class="mb-4">
-          <add-pilot-card :card-height="cardHeight" />
-        </v-col>
-      </v-row>
-    </v-container>
-  </div>
+    <v-slide-x-transition mode="out-in">
+      <v-container v-if="listOption === 0" fluid>
+        <v-row>
+          <v-col v-for="p in pilots" :key="p.id" cols="12">
+            <pilot-card :pilot="p" />
+          </v-col>
+        </v-row>
+      </v-container>
+
+      <pilot-table v-else :pilots="pilots" />
+    </v-slide-x-transition>
+    <!-- <v-col xs3 class="mb-4">
+          <add-pilot-card :card-height="300" />
+    </v-col>-->
+  </v-container>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
-import { Pilot } from '@/class'
-import PilotCard from './PilotCard.vue'
+import RosterSort from './components/RosterSort.vue'
+import PilotCard from './components/PilotCard.vue'
+import PilotTable from './components/PilotTable.vue'
 import AddPilotCard from './AddPilotCard.vue'
 
 export default Vue.extend({
   name: 'roster-view',
-  components: { PilotCard, AddPilotCard },
+  components: { RosterSort, PilotCard, PilotTable, AddPilotCard },
   data: () => ({
-    sorts: [
-      { name: 'Created', field: '' },
-      { name: 'Callsign', field: 'Callsign' },
-      { name: 'Name', field: 'Name' },
-      { name: 'License Level', field: 'Level' },
-    ],
-    currentSort: { name: 'Created', field: '' },
-    ascending: false,
-    cardHeight: 300,
+    pilots: [],
+    listOption: 0,
   }),
-  computed: {
-    pilots(): Pilot[] {
-      const vm = this
-      var allPilots = vm.$store.getters['getAllPilots']
-      if (vm.currentSort && vm.currentSort.field !== '') {
-        allPilots =
-          vm.currentSort.field === 'Level'
-            ? vm.$_.sortBy(allPilots, vm.currentSort.field)
-            : vm.$_.sortBy(allPilots, p => p[vm.currentSort.field].toLowerCase())
-      }
-      if (!vm.ascending) {
-        return vm.$_.reverse(vm.$_.clone(allPilots))
-      }
-      return allPilots
-    },
+  created() {
+    this.pilots = this.$store.getters.getAllPilots
   },
   methods: {
-    sortBy(sort: { name: string; field: string }, isAscending: boolean) {
-      this.currentSort = sort
-      this.ascending = isAscending
+    sortPilots(sortedPilots) {
+      this.pilots = sortedPilots
     },
   },
 })
