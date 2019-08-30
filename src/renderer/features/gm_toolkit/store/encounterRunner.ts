2e599675efba2f278f379b389575a1b3723e4203
Qwertronix <qwertronix@gmail.com>
2019-08-30 02:48:21 -0400

    refactored encounter runner store

--- src/renderer/features/gm_toolkit/store/encounterRunner.ts
+++ src/renderer/features/gm_toolkit/store/encounterRunner.ts
@@ -3,6 +3,7 @@ import ActiveEncounter from '../logic/ActiveEncounter'
 import _ from 'lodash'
 import io from '../../_shared/data_io'
 import EncounterBase from '../logic/EncounterBase'
+import { VuexModule, Module, Action, Mutation } from 'vuex-module-decorators';
 
 function saveEncounters(encounters: ActiveEncounter[]) {
   const serialized = encounters.map(x => x.serialize())
@@ -11,42 +12,50 @@ function saveEncounters(encounters: ActiveEncounter[]) {
   })
 }
 
-export default {
+
+@Module({
+  name: "encounterRunner",
   namespaced: true,
-  state: {
-    activeEncounters: [],
-  },
-  mutations: {
-    load(state: any) {
-      state.activeEncounters = io
-        .loadUserData(Vue.prototype.userDataPath, 'active_encounters.json')
-        .map(x => new ActiveEncounter(x))
-      saveEncounters(state.activeEncounters)
-    },
-    add(state: any, enc: ActiveEncounter) {
-      state.activeEncounters.push(enc)
-      saveEncounters(state.activeEncounters)
-    },
-    delete(state: any, id: string) {
-      _.remove(state.activeEncounters, { id })
-      saveEncounters(state.activeEncounters)
-    },
-    edit(state: any, newEncounter: ActiveEncounter) {
-      const target = state.activeEncounters.find(
-        (enc: ActiveEncounter) => enc.id === newEncounter.id
-      )
-      if (!target) throw new Error('encounter does not exist')
-      else {
-        Object.assign(target, newEncounter)
-        _.debounce(saveEncounters, 300)(state.activeEncounters)
-      }
-    },
-  },
-  actions: {
-    runEncounter({ state, commit }: any, baseEnc: EncounterBase) {
-      const activeEnc = new ActiveEncounter(baseEnc)
-      commit('add', activeEnc)
-      return activeEnc
-    },
-  },
+})
+export class EncounterRunnerStore extends VuexModule {
+  activeEncounters: ActiveEncounter[] = []
+
+  @Mutation
+  load() {
+    this.activeEncounters = io
+      .loadUserData(Vue.prototype.userDataPath, 'active_encounters.json')
+      .map(x => new ActiveEncounter(x))
+    saveEncounters(this.activeEncounters)
+  }
+
+  @Mutation
+  add(enc: ActiveEncounter) {
+    this.activeEncounters.push(enc)
+    saveEncounters(this.activeEncounters)
+  }
+
+  @Mutation
+  delete(id: string) {
+    _.remove(this.activeEncounters, { id })
+    saveEncounters(this.activeEncounters)
+  }
+
+  @Mutation
+  edit(newEncounter: ActiveEncounter) {
+    const target = this.activeEncounters.find(
+      (enc: ActiveEncounter) => enc.id === newEncounter.id
+    )
+    if (!target) throw new Error('encounter does not exist')
+    else {
+      Object.assign(target, newEncounter)
+      _.debounce(saveEncounters, 300)(this.activeEncounters)
+    }
+  }
+
+  @Action
+  runEncounter(baseEnc: EncounterBase) {
+    const activeEnc = new ActiveEncounter(baseEnc)
+    this.context.commit('add', activeEnc)
+    return activeEnc
+  }
 }
