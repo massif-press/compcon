ca84b7d82b196342a2fa3d3fbe7c28601396a600
ari <sndaritsune@gmail.com>
2019-12-02 00:56:58 -0300

    change start_url again...

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -121,7 +121,7 @@ module.exports = {
         new WebpackPwaManifest({
             name: 'COMP/CON',
             short_name: 'C/C',
-            start_url: '/',
+            start_url: '/index.html',
             scope: '.',
             description: 'Digital toolkit for the LANCER RPG',
             background_color: '#ffffff',
