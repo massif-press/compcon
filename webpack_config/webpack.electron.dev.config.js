d17754c68e647ac60d876b4aab1200ca0b351ad6
ari <sndaritsune@gmail.com>
2019-12-11 03:38:43 -0300

    add electron hot reload dev

---
+++ webpack_config/webpack.electron.dev.config.js
@@ -0,0 +1,11 @@
+const WriteFilePlugin = require('write-file-webpack-plugin')
+
+module.exports = {
+    devServer: {
+        hot: false,
+        open: false
+    },
+    plugins: [
+        new WriteFilePlugin()
+    ]
+}
