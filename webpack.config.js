1165fcea23072be228c8378a44545746c5363342
ari <sndaritsune@gmail.com>
2019-12-12 11:58:36 -0300

    more eslint tweaks

--- webpack.config.js
+++ webpack.config.js
@@ -7,133 +7,133 @@ const path = require('path');
 const merge = require('webpack-merge')
 
 const baseConfig = {
-    mode: process.env.NODE_ENV || 'development',
-    entry: './src/renderer/main.ts',
-    output: {
-        filename: 'bundle.js',
-        publicPath: '',
-    },
-    devServer: {
-        port: 8080,
-        hot: true,
-        open: true,
-        historyApiFallback: true
-    },
-    devtool: 'inline-source-map',
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                exclude: /node_modules/,
-                use: [
-                    {
-                        loader: 'ts-loader',
-                        options: {
-                            appendTsSuffixTo: [/\.vue$/],
-                            transpileOnly: true
-                        }
-                    }
-                ],
-            },
-            // Need babel-loader to transpile for iOS compatibility, doesn't seem like ts-loader can achieve this on its own...?
-            // Should investigate
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: [
-                    {
-                        loader: 'babel-loader',
-                        options: {
-                            "presets": [
-                                [
-                                    "@babel/preset-env",
-                                    {
-                                        corejs: 3,
-                                        useBuiltIns: "entry"
-                                    }
-                                ],
-                            ]
-                        }
-                    }
-                ]
-            },
-            {
-                test: /\.vue$/,
-                loader: 'vue-loader'
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    'vue-style-loader',
-                    'css-loader'
-                ]
-            },
-            {
-                test: /\.(woff|woff2|ttf|otf|eot)$/,
-                loader: 'file-loader',
-                options: {
-                    outputPath: 'font',
-                    name: '[hash].[ext]',
-                    esModule: false,
-                }
-            },
-            {
-                test: /\.(png|jpg|gif|svg)$/,
-                loader: 'file-loader',
-                options: {
-                    esModule: false,
-                    outputPath: 'img',
-                    name: '[name].[ext]?[hash]'
-                }
-            },
-            {
-                test: /\.txt$/,
-                loader: 'raw-loader',
-                options: {
-                    esModule: false,
-                }
+  mode: process.env.NODE_ENV || 'development',
+  entry: './src/renderer/main.ts',
+  output: {
+    filename: 'bundle.js',
+    publicPath: '',
+  },
+  devServer: {
+    port: 8080,
+    hot: true,
+    open: true,
+    historyApiFallback: true
+  },
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'ts-loader',
+            options: {
+              appendTsSuffixTo: [/\.vue$/],
+              transpileOnly: true
             }
+          }
         ],
-    },
-    resolve: {
-        extensions: ['.tsx', '.ts', '.vue', '.js'],
-        alias: {
-            '@': path.resolve('src/renderer'),
-            '@assets': path.resolve('src/renderer/assets')
+      },
+      // Need babel-loader to transpile for iOS compatibility, doesn't seem like ts-loader can achieve this on its own...?
+      // Should investigate
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              "presets": [
+                [
+                  "@babel/preset-env",
+                  {
+                    corejs: 3,
+                    useBuiltIns: "entry"
+                  }
+                ],
+              ]
+            }
+          }
+        ]
+      },
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'vue-style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.(woff|woff2|ttf|otf|eot)$/,
+        loader: 'file-loader',
+        options: {
+          outputPath: 'font',
+          name: '[hash].[ext]',
+          esModule: false,
         }
-    },
-    plugins: [
-        new CleanWebpackPlugin(),
-        new VueLoaderPlugin(),
-        new ForkTsCheckerWebpackPlugin(),
-        new HTMLWebpackPlugin({
-            showErrors: true,
-            cache: false,
-            title: 'COMP/CON',
-            favicon: path.resolve(__dirname, './icons/icon.ico'),
-            template: path.resolve(__dirname, 'public/index.html')
-        }),
-    ]
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: 'file-loader',
+        options: {
+          esModule: false,
+          outputPath: 'img',
+          name: '[name].[ext]?[hash]'
+        }
+      },
+      {
+        test: /\.txt$/,
+        loader: 'raw-loader',
+        options: {
+          esModule: false,
+        }
+      }
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.vue', '.js'],
+    alias: {
+      '@': path.resolve('src/renderer'),
+      '@assets': path.resolve('src/renderer/assets')
+    }
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new VueLoaderPlugin(),
+    new ForkTsCheckerWebpackPlugin(),
+    new HTMLWebpackPlugin({
+      showErrors: true,
+      cache: false,
+      title: 'COMP/CON',
+      favicon: path.resolve(__dirname, './icons/icon.ico'),
+      template: path.resolve(__dirname, 'public/index.html')
+    }),
+  ]
 }
 
 function requireIfExists(filePath) {
-    console.log(filePath)
-    try {
-        return require(filePath)
-    } catch (err) {
-        return {}
-    }
+  console.log(filePath)
+  try {
+    return require(filePath)
+  } catch (err) {
+    return {}
+  }
 }
 
 module.exports = function (env) {
 
-    const target = env.prod ? 'prod' : 'dev'
+  const target = env.prod ? 'prod' : 'dev'
 
-    const out = merge(
-        baseConfig,
-        requireIfExists(`./webpack_config/webpack.${target}.config`),
-        requireIfExists(`./webpack_config/webpack.${env.platform}.config`),
-        requireIfExists(`./webpack_config/webpack.${env.platform}.${target}.config`),
-    )
-    return out
+  const out = merge(
+    baseConfig,
+    requireIfExists(`./webpack_config/webpack.${target}.config`),
+    requireIfExists(`./webpack_config/webpack.${env.platform}.config`),
+    requireIfExists(`./webpack_config/webpack.${env.platform}.${target}.config`),
+  )
+  return out
 }
