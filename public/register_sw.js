86500932a2c1b6dd9cdad1631003c7b3e52473da
ari <sndaritsune@gmail.com>
2019-12-12 11:54:33 -0300

    style fixes

--- public/register_sw.js
+++ public/register_sw.js
@@ -1,12 +1,12 @@
 if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-        navigator.serviceWorker
-            .register('/sw.js')
-            .then(registration => {
-                console.log('SW registered: ', registration)
-            })
-            .catch(registrationError => {
-                console.log('SW registration failed: ', registrationError)
-            })
-    })
+  window.addEventListener('load', () => {
+    navigator.serviceWorker
+      .register('/sw.js')
+      .then(registration => {
+        console.log('SW registered: ', registration)
+      })
+      .catch(registrationError => {
+        console.log('SW registration failed: ', registrationError)
+      })
+  })
 }
--- webpack_config/webpack.electron.config.js
+++ webpack_config/webpack.electron.config.js
@@ -1,12 +1,12 @@
 const path = require('path');
 
 module.exports = {
-    output: {
-        path: path.resolve(__dirname, '../electron/app'),
-    },
-    target: "electron-main",
-    node: {
-        fs: 'empty',
-        __dirname: false
-    },
+  output: {
+    path: path.resolve(__dirname, '../electron/app'),
+  },
+  target: "electron-main",
+  node: {
+    fs: 'empty',
+    __dirname: false
+  },
 }
