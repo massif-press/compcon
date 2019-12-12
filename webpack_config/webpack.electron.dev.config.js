d50488c115f59ea7f48a622168a12a4f59459ee2
ari <sndaritsune@gmail.com>
2019-12-12 16:50:52 -0300

    add electron dev subprocess

--- webpack_config/webpack.electron.dev.config.js
+++ webpack_config/webpack.electron.dev.config.js
@@ -1,11 +1,40 @@
 const WriteFilePlugin = require('write-file-webpack-plugin')
+const childProcess = require('child_process')
+const path = require('path')
+
+const subprocessPlugin = {
+  apply(compiler) {
+    compiler.hooks.done.tapAsync(
+      'ElectronSubprocess',
+      (compilation, callback) => {
+
+        const platform = require('os').platform()
+        const childArgs = platform === 'win32' ?
+          ['cmd', ['/C', path.resolve('electron/node_modules/.bin/electron.cmd' + ' ./')]]
+          : [path.resolve('electron/node_modules/.bin/electron'), './']
+
+        const child = childProcess.spawn(...childArgs, {
+          cwd: './electron',
+        })
+        child.stdout.on('data', function (data) {
+          console.log(data.toString());
+        });
+        child.stderr.on('data', function (data) {
+          console.log(data.toString());
+        });
+        callback()
+      }
+    )
+  }
+}
 
 module.exports = {
   devServer: {
-    hot: false,
+    // hot: false,
     open: false
   },
   plugins: [
-    new WriteFilePlugin()
+    new WriteFilePlugin(),
+    subprocessPlugin
   ]
 }
