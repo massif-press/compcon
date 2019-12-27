6d658d93293e6558c3defd1ce74e6a7a3b226620
ari <sndaritsune@gmail.com>
2019-12-27 10:58:44 -0300

    clean up travis setup for production

--- build_scripts/deploy_butler.sh
+++ build_scripts/deploy_butler.sh
@@ -1,21 +1,18 @@
 #!/bin/bash
 
-export TAG_BUILD_VERSION=`echo $TRAVIS_TAG | cut -d'-' -f1`
+TAG_BUILD_VERSION=`echo $TRAVIS_TAG | cut -d'-' -f1`
 build_type=`echo $TRAVIS_TAG | cut -d'-' -f2`
 
-if [ `echo $TRAVIS_TAG | cut -d'-' -f2` == "stable" ]
-then
-    export MAC_ITCH_CHANNEL="massif-press/compcon:osx"
-    export WIN_ITCH_CHANNEL="massif-press/compcon:win"
-    export LINUX_ITCH_CHANNEL="massif-press/compcon:linux"
-else
-    export MAC_ITCH_CHANNEL="massif-press/compcon:osx-${build_type}"
-    export WIN_ITCH_CHANNEL="massif-press/compcon:win-${build_type}"
-    export LINUX_ITCH_CHANNEL="massif-press/compcon:linux-${build_type}"
-fi
 
-curl -L -o butler.zip https://broth.itch.ovh/butler/darwin-amd64/LATEST/archive/default &&
+MAC_ITCH_CHANNEL="massif-press/compcon:osx"
+WIN_ITCH_CHANNEL="massif-press/compcon:win"
+LINUX_ITCH_CHANNEL="massif-press/compcon:linux"
+
+
+curl -L -o butler.zip https://broth.itch.ovh/butler/linux-amd64/LATEST/archive/default &&
 unzip butler.zip &&
 chmod +x butler &&
 ./butler -V &&
-./butler push ./electron/dist/COMPCON*.exe massif-press/compcon:test-hidden --userversion 0.0.0
+./butler push ./electron/dist/COMPCON*.exe ${WIN_ITCH_CHANNEL} --userversion ${TAG_BUILD_VERSION} &&
+./butler push ./electron/dist/COMPCON*mac.zip ${MAC_ITCH_CHANNEL} --userversion ${TAG_BUILD_VERSION} &&
+./butler push ./electron/dist/COMPCON*.AppImage ${LINUX_ITCH_CHANNEL} --userversion ${TAG_BUILD_VERSION}
