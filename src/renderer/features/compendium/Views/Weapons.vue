b4c4b229d6f3e414fa19970391e6bc9ee0ddfde5
ari <sndaritsune@gmail.com>
2019-12-28 17:33:07 -0300

    reactive compendium pages

--- src/renderer/features/compendium/Views/Weapons.vue
+++ src/renderer/features/compendium/Views/Weapons.vue
@@ -6,28 +6,32 @@
 
 <script lang="ts">
 import Vue from 'vue'
+import Component from 'vue-class-component'
 import CompendiumTable from '../UI/CompendiumTable.vue'
 import { getModule } from 'vuex-module-decorators'
 import { CompendiumStore } from '@/store'
+import { MechWeapon } from '../../../classes/mech/MechWeapon'
 
-export default Vue.extend({
-  name: 'weapons',
-  components: { CompendiumTable },
-  data: () => ({
-    headers: [
-      { text: 'Source', align: 'left', value: 'Source' },
-      { text: 'Weapon', align: 'left', value: 'Name' },
-      { text: 'License', align: 'left', value: 'LicenseString' },
-      { text: 'Size', align: 'left', value: 'Size' },
-      { text: 'Type', align: 'left', value: 'Type' },
-      { text: 'Range', align: 'left', value: 'Range' },
-      { text: 'Damage', align: 'left', value: 'Damage' },
-    ],
-    weapons: [],
-  }),
-  created() {
-    const compendium = getModule(CompendiumStore, this.$store)
-    this.weapons = compendium.MechWeapons.filter(x => x.Source)
-  },
+
+@Component({
+  components: { CompendiumTable }
 })
+export default class Weapons extends Vue {
+
+  public headers = [
+    { text: 'Source', align: 'left', value: 'Source' },
+    { text: 'Weapon', align: 'left', value: 'Name' },
+    { text: 'License', align: 'left', value: 'LicenseString' },
+    { text: 'Size', align: 'left', value: 'Size' },
+    { text: 'Type', align: 'left', value: 'Type' },
+    { text: 'Range', align: 'left', value: 'Range' },
+    { text: 'Damage', align: 'left', value: 'Damage' },
+  ]
+
+  private compendium = getModule(CompendiumStore, this.$store)
+  public get weapons(): MechWeapon[] {
+    return this.compendium.MechWeapons.filter(x => x.Source)
+  }
+
+}
 </script>
