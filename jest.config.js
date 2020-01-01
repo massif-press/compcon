e32dbb87e68d311722badd2a47f1aafac0f2d85f
ari <sndaritsune@gmail.com>
2020-01-01 01:12:10 -0300

    Remove src/main and move src/renderer/* to src/

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,6 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/src/renderer/$1',
+    '^@/(.*)$': '<rootDir>/src/$1',
   },
 }
