d519e95a117e20adab7f19ea1b3918bc86b57802
John Arena <john.arena@qsrsoft.com>
2020-01-06 11:29:42 -0600

    starting on encounter runner

---
+++ src/classes/encounter/ActiveMission.ts
@@ -0,0 +1,13 @@
+class ActiveMission {
+  public get CurrentStep(): number {
+    return 0
+  }
+
+  public CurrentEncounter(): string {
+    return 'encounter name (x/encounters.length)'
+  }
+
+  public CurrentTurn(): number {
+    return 0
+  }
+}
