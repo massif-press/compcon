86d71949935e6f58c09da67edf7060f7824e4f59
John Arena <john.arena@qsrsoft.com>
2020-01-07 13:46:35 -0600

    finish briefing page

---
+++ src/classes/npc/interfaces.ts
@@ -0,0 +1,24 @@
+import { INpcFeatureData } from './NpcFeature'
+import { INpcReactionData } from './NpcReaction'
+import { INpcSystemData } from './NpcSystem'
+import { INpcTechData } from './NpcTech'
+import { INpcItemSaveData } from './NpcItem'
+import { INpcDamageData, INpcWeaponData } from './NpcWeapon'
+import { INpcStats } from './NpcStats'
+import { INpcClassData } from './NpcClass'
+import { INpcTemplateData } from './NpcTemplate'
+import { INpcData } from './Npc'
+
+export {
+  INpcFeatureData,
+  INpcReactionData,
+  INpcSystemData,
+  INpcDamageData,
+  INpcWeaponData,
+  INpcStats,
+  INpcClassData,
+  INpcTemplateData,
+  INpcItemSaveData,
+  INpcTechData,
+  INpcData,
+}
