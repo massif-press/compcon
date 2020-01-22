67a1f02287eed46dff905719f961dd63bd75b9ed
John Arena <john.arena@qsrsoft.com>
2020-01-22 16:29:34 -0600

    even more bugfixes

---
+++ src/ui/components/CCComboSelect.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-menu v-model="menu" offset-y :close-on-content-click="false">
+    <template v-slot:activator="{ on }">
+      <v-icon small class="fadeSelect" v-on="on">mdi-circle-edit-outline</v-icon>
+    </template>
+    <v-card outlined>
+      <v-card-text>
+        <v-combobox
+          v-model="sel"
+          autofocus
+          background-color="stark-panel"
+          dense
+          outlined
+          hide-details
+          :items="items"
+          item-value="value"
+        />
+      </v-card-text>
+    </v-card>
+  </v-menu>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  name: 'cc-short-string-editor',
+  props: {
+    items: {
+      type: Array,
+      required: true,
+    },
+  },
+  data: () => ({
+    sel: '',
+    menu: false,
+  }),
+  watch: {
+    sel() {
+      const s = typeof this.sel === 'string' ? this.sel : this.sel.value
+      this.$emit('set', s)
+    },
+  },
+})
+</script>
