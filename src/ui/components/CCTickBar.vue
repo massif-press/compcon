bf2454486581f44d254715e3496583b148dd395b
Qwertronix <qwertronix@gmail.com>
2020-01-25 14:35:39 -0500

    refactor: tickbar vcc conversion

--- src/ui/components/CCTickBar.vue
+++ src/ui/components/CCTickBar.vue
@@ -54,146 +54,125 @@
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
-export default Vue.extend({
-  name: 'tick-bar',
-  props: {
-    label: {
-      type: String,
-      required: false,
-      default: '',
-    },
-    justify: {
-      type: String,
-      required: false,
-      default: 'start',
-    },
-    current: {
-      type: Number,
-      required: true,
-    },
-    max: {
-      type: Number,
-      required: true,
-    },
-    small: {
-      type: Boolean,
-    },
-    large: {
-      type: Boolean,
-    },
-    emptyIcon: {
-      type: String,
-      required: false,
-      default: 'mdi-hexagon-outline',
-    },
-    fullIcon: {
-      type: String,
-      required: false,
-      default: 'mdi-hexagon',
-    },
-    color: {
-      type: String,
-      required: false,
-      default: 'primary',
-    },
-    bgColor: {
-      type: String,
-      required: false,
-      default: 'grey lighten-1',
-    },
-    readonly: {
-      type: Boolean,
-    },
-    noInput: {
-      type: Boolean,
-    },
-    flipInput: {
-      type: Boolean,
-    },
-    noPips: {
-      type: Boolean,
-    },
-    clearable: {
-      type: Boolean,
-    },
-    rollover: { type: Boolean, default: false },
-    rolloverNegative: { type: Boolean, default: false },
-  },
-  data: () => ({
-    model: 0,
-    lock: true,
-    inputting: false,
-    myInput: '',
-  }),
-  watch: {
-    model(val: number) {
-      if (!this.lock && !isNaN(val)) {
-        this.$emit('update', val)
-      }
-    },
-  },
+import { Vue, Component, Prop, Watch, Ref } from 'vue-property-decorator'
+
+@Component({ name: 'tick-bar', })
+export default class CCTickBar extends Vue {
   created() {
     this.lock = true
     if (!this.readonly) {
       this.model = this.current > this.max ? this.max : this.current
     } else this.model = this.max
     this.lock = false
-  },
-  methods: {
-    startInputting() {
-      this.inputting = true
-      this.$nextTick(() => {
-        ;(this.$refs.pipinput as HTMLInputElement).focus()
-      })
-    },
-    sendInput() {
-      const thisInput = this.myInput
-      if (!thisInput.match(/\d/)) return
+  }
 
-      this.inputting = false
+  @Prop({ type: String, required: false, default: '', })
+  label: string
 
-      let preResult = this.current
+  @Prop({ type: String, required: false, default: 'start', })
+  justify: string
+  @Prop({ type: Boolean, })
+  small?: boolean
+  @Prop({ type: Boolean, })
+  large?: boolean
+  @Prop({ type: String, required: false, default: 'mdi-hexagon-outline', })
+  emptyIcon: string
+  @Prop({ type: String, required: false, default: 'mdi-hexagon', })
+  fullIcon: string
+  @Prop({ type: String, required: false, default: 'primary', })
+  color: string
+  @Prop({ type: String,required: false, default: 'grey lighten-1', })
+  bgColor: string
+  @Prop({ type: Boolean, })
+  noPips?: boolean
 
-      if (thisInput === '') return
-      else if (thisInput.startsWith('+')) {
-        preResult += parseInt(thisInput.substr(1))
-      } else if (thisInput.startsWith('-')) {
-        preResult -= parseInt(thisInput.substr(1))
-      } else {
-        preResult = parseInt(thisInput)
+  @Prop({ type: Boolean, })
+  readonly?: boolean
+  @Prop({ type: Boolean, })
+  noInput?: boolean
+  @Prop({ type: Boolean, })
+  flipInput?: boolean
+  @Prop({ type: Boolean, })
+  clearable?: boolean
+  @Prop({ type: Boolean, default: false })
+  rollover: boolean
+  @Prop({ type: Boolean, default: false })
+  rolloverNegative: boolean
+
+  @Prop({ type: Number, required: true, })
+  current!: number
+  @Prop({ type: Number, required: true, })
+  max!: number
+
+  model = 0
+  lock = true
+  inputting = false
+  myInput = ''
+
+  @Watch('model')
+  onModelChange(val: number, oldVal: number) {
+    if (!this.lock && !isNaN(val)) {
+      this.$emit('update', val)
+    }
+  }
+
+  onInputChange(e) {
+    const newVal = e.target.value
+    if (newVal.match(/^[+-\d]\d*$/) || newVal === '') this.myInput = newVal
+    else e.target.value = this.myInput
+  }
+
+  @Ref('pipinput') readonly pipinput: HTMLInputElement
+  startInputting() {
+    this.inputting = true
+    this.$nextTick(() => {
+      this.pipinput.focus()
+    })
+  }
+
+  sendInput() {
+    const thisInput = this.myInput
+    if (!thisInput.match(/\d/)) return
+
+    this.inputting = false
+
+    let preResult = this.current
+
+    if (thisInput === '') return
+    else if (thisInput.startsWith('+')) {
+      preResult += parseInt(thisInput.substr(1))
+    } else if (thisInput.startsWith('-')) {
+      preResult -= parseInt(thisInput.substr(1))
+    } else {
+      preResult = parseInt(thisInput)
+    }
+
+    if (this.rolloverNegative) {
+      while (preResult > this.max) {
+        preResult = preResult - this.max
+        this.$emit('rollover')
       }
+    } else preResult = Math.min(preResult, this.max)
 
-      if (this.rolloverNegative) {
-        while (preResult > this.max) {
-          preResult = preResult - this.max
-          this.$emit('rollover')
-        }
-      } else preResult = Math.min(preResult, this.max)
-
-      if (this.rollover) {
-        while (preResult < 1) {
-          preResult = this.max + preResult
-          this.$emit('rollover')
-        }
-      } else {
-        preResult = Math.max(0, preResult)
+    if (this.rollover) {
+      while (preResult < 1) {
+        preResult = this.max + preResult
+        this.$emit('rollover')
       }
+    } else {
+      preResult = Math.max(0, preResult)
+    }
 
-      this.$emit('update', preResult)
-      this.myInput = ''
-    },
-    cancelInput() {
-      this.inputting = false
-      this.myInput = ''
-    },
-    onInputChange(e) {
-      const newVal = e.target.value
-      if (newVal.match(/^[+-\d]\d*$/) || newVal === '') this.myInput = newVal
-      else e.target.value = this.myInput
-    },
-  },
-})
+    this.$emit('update', preResult)
+    this.myInput = ''
+  }
+  
+  cancelInput() {
+    this.inputting = false
+    this.myInput = ''
+  }
+
+}
 </script>
 
 <style scoped>
