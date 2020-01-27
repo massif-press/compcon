616724f420fb837c217e7e3e777648241bee9894
Ari <sndaritsune@gmail.com>
2020-01-27 01:15:34 -0300

    refactor(typescript): move vue type augmentations to their own file

---
+++ src/shims/shims-vue.d.ts
@@ -0,0 +1,10 @@
+import Vue from 'vue'
+
+declare module 'vue/types/vue' {
+  interface Vue {
+    $platform: string
+    lancerVersion: string
+    $notify: (text: string, type?: string) => void
+  }
+
+}
