03a5648a5355e7a09c66fb607c2342e7224f8f9d
John Arena <john.arena@qsrsoft.com>
2020-01-30 17:55:42 -0600

    missed files

---
+++ src/features/nav/pages/SupporterBadges/TierThree.vue
@@ -0,0 +1,10 @@
+<template>
+  <v-chip x-large label color="teal accent-4" class="mx-2">Firstname Lastname</v-chip>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  name: 'dev-badge',
+})
+</script>
