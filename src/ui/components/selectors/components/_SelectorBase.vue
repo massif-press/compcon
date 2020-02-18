c99147beec227c4b57951fc0dd00970d9e1e266a
John Arena <john.arena@qsrsoft.com>
2020-02-18 16:01:44 -0600

    feat(Pilot Sheet): single-scroll Pilot skill/talent/bonus pages
    Changed the Pilot Skill/Talent/License/CB selector pages to remove the inset scroll area and float
    the selection panel depeding on scroll position
    
    Closes #525

--- src/ui/components/selectors/components/_SelectorBase.vue
+++ src/ui/components/selectors/components/_SelectorBase.vue
@@ -1,11 +1,18 @@
 <template>
   <v-container fluid style="overflow: hidden">
     <v-row>
-      <v-col cols="3" :class="`pr-4 ${success ? 'bordered-success' : 'bordered-primary'}`">
-        <cc-title small :color="success ? 'success' : 'primary'">{{ title }}</cc-title>
-        <slot name="left-column" />
+      <v-col cols="3" style="position: relative" class="pr-4">
+        <div
+          :style="`position: fixed; width: 23vw; transform: translateY(-${offsetTop}px)`"
+          :class="success ? 'bordered-success' : 'bordered-primary'"
+        >
+          <div width="90%" class="ml-3">
+            <cc-title small :color="success ? 'success' : 'primary'">{{ title }}</cc-title>
+            <slot name="left-column" />
+          </div>
+        </div>
       </v-col>
-      <v-col cols="9" style="height: 90vh; overflow-y: scroll">
+      <v-col cols="9">
         <slot name="right-column" />
       </v-col>
     </v-row>
@@ -17,6 +24,7 @@ import Vue from 'vue'
 
 export default Vue.extend({
   name: 'selector',
+
   props: {
     title: {
       type: String,
@@ -31,6 +39,22 @@ export default Vue.extend({
       type: Boolean,
       required: false,
     },
+  },
+  data: () => ({
+    offsetTop: 0,
+  }),
+  mounted() {
+    window.addEventListener(
+      'scroll',
+      () => {
+        const scrollPos = window.scrollY
+        this.offsetTop = scrollPos > 400 ? 400 : scrollPos
+      },
+      true
+    )
+  },
+  beforeDestroy() {
+    window.removeEventListener('scroll', null, false)
   },
 })
 </script>
