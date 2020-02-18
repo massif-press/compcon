d734a5f8a67a95de589602329e14462e1b98a702
John Arena <john.arena@qsrsoft.com>
2020-02-18 14:43:58 -0600

    feat(Compendium): add tabs to Reserves & Downtime Compendium page
    Rewrites the Reserves & Downtime page in the compendium to look like the Reserves selector tabbed
    screen, instead of one undifferentiated list
    
    Closes #641

--- src/features/compendium/Views/Reserves.vue
+++ src/features/compendium/Views/Reserves.vue
@@ -1,23 +1,40 @@
 <template>
   <div>
     <h1 class="heading mb-3 ml-5">RESERVES & DOWNTIME</h1>
-    <span class="heading mech">Reserves</span>
-    <v-container grid-list-md fluid>
-      <v-row wrap fill-height justify="center">
-        <reserve-card v-for="r in reserves" :key="r.id" :reserve="r" />
-      </v-row>
-    </v-container>
-    <span class="heading mech">Downtime Actions</span>
-    <v-container grid-list-md fluid>
-      <v-row wrap fill-height justify="center">
-        <action-card v-for="a in downtimeActions" :key="a.id" :action="a" downtime />
-      </v-row>
-    </v-container>
+    <v-tabs
+      v-model="tabModel"
+      background-color="primary"
+      :slider-size="12"
+      slider-color="active"
+      fixed-tabs
+    >
+      <v-tab v-for="(k, i) in Object.keys(reserves)" :key="k + i" ripple>
+        {{ k }}
+      </v-tab>
+      <v-tab ripple>
+        Downtime Actions
+      </v-tab>
+      <v-tab-item v-for="(k, i) in Object.keys(reserves)" :key="k + i + 'desc'">
+        <v-container grid-list-md fluid>
+          <v-row wrap fill-height justify="center">
+            <reserve-card v-for="r in reserves[k]" :key="r.ID" :reserve="r" />
+          </v-row>
+        </v-container>
+      </v-tab-item>
+      <v-tab-item v-for="(k, i) in Object.keys(reserves)" :key="k + i + 'desc'">
+        <v-container grid-list-md fluid>
+          <v-row wrap fill-height justify="center">
+            <action-card v-for="a in downtimeActions" :key="a.id" :action="a" downtime />
+          </v-row>
+        </v-container>
+      </v-tab-item>
+    </v-tabs>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+import _ from 'lodash'
 import ActionCard from '../components/ActionCard.vue'
 import ReserveCard from '../components/ReserveCard.vue'
 import { actions, reserves } from 'lancer-data'
@@ -26,12 +43,13 @@ export default Vue.extend({
   name: 'reference',
   components: { ActionCard, ReserveCard },
   data: () => ({
+    tabModel: 0,
     downtimeActions: [],
     reserves: [],
   }),
   created() {
+    this.reserves = _.groupBy(reserves, 'type')
     this.downtimeActions = actions.filter(x => x.action_type === 'downtime')
-    this.reserves = reserves
   },
 })
 </script>
