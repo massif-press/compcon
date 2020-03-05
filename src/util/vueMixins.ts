2db4dee20f5afb0e725bf4f4d923be5078b8dc49
John Arena <john.arena@qsrsoft.com>
2020-03-05 11:48:01 -0600

    fix(Main Menu): remove broken screen transition effect
    something with the router transition effect was breaking (possibly due to a package update). Removed
    it for the time being

--- src/util/vueMixins.ts
+++ src/util/vueMixins.ts
@@ -1,8 +1,10 @@
 import Vue, { VueConstructor } from 'vue'
 
-export default function vueMixins<T extends VueConstructor[]> (...args: T): ExtractVue<T> extends infer V ? V extends Vue ? VueConstructor<V> : never : never
-export default function vueMixins<T extends Vue> (...args: VueConstructor[]): VueConstructor<T>
-export default function vueMixins (...args: VueConstructor[]): VueConstructor {
+export default function vueMixins<T extends VueConstructor[]>(
+  ...args: T
+): ExtractVue<T> extends infer V ? (V extends Vue ? VueConstructor<V> : never) : never
+export default function vueMixins<T extends Vue>(...args: VueConstructor[]): VueConstructor<T>
+export default function vueMixins(...args: VueConstructor[]): VueConstructor {
   return Vue.extend({ mixins: args })
 }
 
@@ -11,10 +13,13 @@ export default function vueMixins (...args: VueConstructor[]): VueConstructor {
  * Useful for adding types when using mixins().extend()
  */
 export type ExtractVue<T extends VueConstructor | VueConstructor[]> = T extends (infer U)[]
-  ? UnionToIntersection<
-    U extends VueConstructor<infer V> ? V : never
-  >
-  : T extends VueConstructor<infer V> ? V : never
+  ? UnionToIntersection<U extends VueConstructor<infer V> ? V : never>
+  : T extends VueConstructor<infer V>
+  ? V
+  : never
 
-type UnionToIntersection<U> =
-  (U extends any ? (k: U) => void : never) extends ((k: infer I) => void) ? I : never
+type UnionToIntersection<U> = (U extends any
+? (k: U) => void
+: never) extends (k: infer I) => void
+  ? I
+  : never
