dc4a55f9d7d6999eb63e2491eb6f46ae127ee6a1
John Arena <john.arena@qsrsoft.com>
2020-03-06 14:10:41 -0600

    fix: item action hotfix
    fixed some code that was causing item actions to display incorrectly

--- src/classes/effects/TechEffect.ts
+++ src/classes/effects/TechEffect.ts
@@ -10,6 +10,7 @@ interface IInvadeOptionData {
 interface ITechEffectData extends IEffectData {
   detail: string
   options?: IInvadeOptionData[]
+  option_set?: string
 }
 
 class InvadeOption {
@@ -17,23 +18,25 @@ class InvadeOption {
   public readonly Detail: string
   public readonly Activation: ActivationType
 
-  public constructor(data: IInvadeOptionData) {
+  public constructor(data: IInvadeOptionData, activation: ActivationType) {
     this.Name = data.name
     this.Detail = data.detail
-    this.Activation = data.activation || ActivationType.Quick
+    this.Activation = activation || ActivationType.Quick
   }
 }
 
 class TechEffect extends ItemEffect {
   public readonly Name?: string
   public readonly Detail: string
+  public readonly OptionSet?: string
   public readonly Options?: InvadeOption[]
 
   public constructor(data: ITechEffectData, err?: boolean) {
     super(err)
     this.Name = data.name
     this.Detail = data.detail
-    this.Options = data.options ? data.options.map(x => new InvadeOption(x)) : []
+    this.OptionSet = data.option_set
+    this.Options = data.options ? data.options.map(x => new InvadeOption(x, data.activation)) : []
     this.activation = data.activation || ActivationType.Quick
     this.effectType = EffectType.Tech
   }
