d7436dcb0bee5bd3d0744b86a6c5ee7477f97e6f
ari <sndaritsune@gmail.com>
2020-04-03 23:53:35 -0300

    use macos butler

--- build_scripts/deploy_butler.sh
+++ build_scripts/deploy_butler.sh
@@ -3,13 +3,11 @@
 TAG_BUILD_VERSION=`echo $TRAVIS_TAG | cut -d'-' -f1`
 build_type=`echo $TRAVIS_TAG | cut -d'-' -f2`
 
-
 MAC_ITCH_CHANNEL="aritsune/compcon-test:mac"
 WIN_ITCH_CHANNEL="aritsune/compcon-test:win"
 LINUX_ITCH_CHANNEL="aritsune/compcon-test:linux"
 
-
-curl -L -o butler.zip https://broth.itch.ovh/butler/linux-amd64/LATEST/archive/default &&
+curl -L -o butler.zip https://broth.itch.ovh/butler/darwin-amd64/LATEST/archive/default &&
 unzip butler.zip &&
 chmod +x butler &&
 ./butler -V &&
