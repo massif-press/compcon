c7f244e9af8f19dac414ccb87ca47342f5094f54
Yri Davies <0xdavy@gmail.com>
2020-04-24 16:42:42 +0200

    Split range and damage output for weapons with multiples

--- src/renderer/io/Roll20.ts
+++ src/renderer/io/Roll20.ts
@@ -81,6 +81,7 @@ interface IRoll20Data {
       name: string
       type: string
       range: number
+      altRanges: string
       damage: string
       damageType: string
       tags: string
@@ -182,8 +183,9 @@ export default function pilotToRoll20(pilot: Pilot, mech: Mech): IRoll20Data {
         name: weapon.Name,
         type: `${weapon.Size} ${weapon.Type}`,
         range: weapon.Range.filter(r => r.Type != RangeType.Blast)[0].Max,
-        damage: weapon.Damage.map(dmg => dmg.Value).join('+'),
-        damageType: weapon.Damage[0].Type,
+        altRanges: weapon.Range.filter(r => r.Type != RangeType.Range).map(r => r.Text).join('|'),
+        damage: weapon.Damage.map(dmg => dmg.Value).join('|'),
+        damageType: weapon.Damage.map(dmg => dmg.Type).join('|'),
         tags: weapon.Tags.map(tag => tag.GetName(pilot.LimitedBonus)).join(', '),
         notes: weapon.Note,
       })),
