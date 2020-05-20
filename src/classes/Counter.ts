e1a26ac0c7af29577b03c3e1c15b64c70533eedc
John Arena <john.arena@qsrsoft.com>
2020-05-20 13:26:47 -0500

    fix(Active Mode): prevent talent counters from being added early
    Closes #849

--- src/classes/Counter.ts
+++ src/classes/Counter.ts
@@ -1,6 +1,8 @@
+/* eslint-disable @typescript-eslint/camelcase */
 interface ICounterData {
   id: string
   name: string
+  level?: number
   min?: number
   max?: number
   default_value?: number
@@ -8,51 +10,54 @@ interface ICounterData {
 }
 
 class Counter {
-
   public readonly ID: string
   public readonly Name: string
+  public readonly Level: number
   public readonly Min: number
   public readonly Max: number
   public readonly Default: number
 
   constructor(data: ICounterData) {
-    const { id, name, min, max, default_value } = data
+    const { id, name, level, min, max, default_value } = data
 
     if (max && default_value > max)
-      throw new Error(`Error creating Counter: Default value of ${default_value} is greater than max value of ${max}`)
-    
+      throw new Error(
+        `Error creating Counter: Default value of ${default_value} is greater than max value of ${max}`
+      )
+
     if (min && default_value < min)
-      throw new Error(`Error creating Counter: Default value of ${default_value} is lesser than min value of ${min}`)
+      throw new Error(
+        `Error creating Counter: Default value of ${default_value} is lesser than min value of ${min}`
+      )
 
     this.ID = id
     this.Name = name
+    this.Level = level || 0
     this.Min = min
     this.Max = max
     this.Default = default_value || 0
 
     this._value = this.Default
-
   }
-
 
   private _value: number
   public get Value(): number {
     return this._value
   }
   public Increment(): void {
-    if (this.Max) this._value = Math.min( this.Max, this._value + 1 )
+    if (this.Max) this._value = Math.min(this.Max, this._value + 1)
     else this._value = this._value + 1
   }
   public Decrement(): void {
-    if (this.Min) this._value = Math.max( this.Min, this._value - 1 )
+    if (this.Min) this._value = Math.max(this.Min, this._value - 1)
     else this._value = this._value - 1
   }
 
   public Set(inputValue: number): void {
     if (typeof inputValue !== 'number' || isNaN(inputValue)) return
     let value = inputValue
-    if (this.Max) value = Math.min( this.Max, value )
-    if (this.Min) value = Math.max( this.Min, value )
+    if (this.Max) value = Math.min(this.Max, value)
+    if (this.Min) value = Math.max(this.Min, value)
     this._value = value
   }
 
@@ -63,15 +68,13 @@ class Counter {
   public Serialize(): ICounterSaveData {
     return {
       id: this.ID,
-      val: this.Value
+      val: this.Value,
     }
   }
 
   public LoadData(data: ICounterSaveData): void {
     this.Set(data.val)
   }
-
-
 }
 
 export { ICounterData, Counter }
