2b61913fc5240c6e2ba24a9eeb29541ed20dc18a
John Arena <john.arena@qsrsoft.com>
2020-05-26 10:47:45 -0500

    feat(Compendium): restyled the compendium landing page
    Cleaner UI and preparation for mobile device support

--- src/features/compendium/Views/Home.vue
+++ src/features/compendium/Views/Home.vue
@@ -1,7 +1,9 @@
 <template>
   <div>
     <v-container fluid>
-      <div class="heading mech text-center mt-n2 mb-n2">COMPENDIUM</div>
+      <div class="display-3 text-center mt-n2 mb-n2" style="letter-spacing: 15px!important">
+        COMPENDIUM
+      </div>
       <v-row justify="center">
         <v-col cols="8">
           <search-bar />
@@ -10,128 +12,166 @@
     </v-container>
     <div class="ml-5 mr-5">
       <v-container fluid grid-list-lg class="mt-0 pt-0">
-        <v-row wrap justify="center">
-          <cc-major-btn
-            :cols="$vuetify.breakpoint.lgAndUp ? '4' : '6'"
+        <v-row dense justify="space-around" align="center">
+          <compendium-page-button
+            lg="4"
+            md="6"
+            sm="12"
             icon="mdi-factory"
             name="Manufacturers"
             @clicked="$router.push('compendium/manufacturers')"
           />
-          <cc-major-btn
-            :cols="$vuetify.breakpoint.lgAndUp ? '4' : '6'"
+          <compendium-page-button
+            lg="4"
+            md="6"
+            sm="12"
             color="primary"
             icon="mdi-view-week"
             name="Licenses"
             @clicked="$router.push('compendium/licenses')"
           />
-          <cc-major-btn
-            :cols="$vuetify.breakpoint.lgAndUp ? '4' : '6'"
+          <compendium-page-button
+            lg="4"
+            md="6"
+            sm="12"
             icon="cci-corebonus"
             name="CORE Bonuses"
             @clicked="$router.push('compendium/corebonuses')"
           />
-          <cc-major-btn
-            :cols="$vuetify.breakpoint.lgAndUp ? '4' : '6'"
+          <compendium-page-button
+            lg="4"
+            md="6"
+            sm="12"
             color="frame"
             icon="cci-frame"
             name="Frames"
             @clicked="$router.push('compendium/frames')"
           />
-          <cc-major-btn
-            :cols="$vuetify.breakpoint.lgAndUp ? '4' : '6'"
+          <compendium-page-button
+            lg="4"
+            md="6"
+            sm="12"
             color="weapon"
             icon="cci-melee"
             name="Mech Weapons"
             @clicked="$router.push('compendium/weapons')"
           />
-          <cc-major-btn
-            :cols="$vuetify.breakpoint.lgAndUp ? '4' : '6'"
+          <compendium-page-button
+            lg="4"
+            md="6"
+            sm="12"
             color="system"
             icon="mdi-chip"
             name="Mech Systems"
             @clicked="$router.push('compendium/systems')"
           />
-          <cc-major-btn
-            :cols="$vuetify.breakpoint.lgAndUp ? '3' : '6'"
+          <compendium-page-button
+            lg="3"
+            md="6"
+            sm="12"
             color="secondary"
             icon="cci-pilot"
             name="Pilot Gear"
             @clicked="$router.push('compendium/pilot_gear')"
           />
-          <cc-major-btn
-            :cols="$vuetify.breakpoint.lgAndUp ? '3' : '6'"
+          <compendium-page-button
+            lg="3"
+            md="6"
+            sm="12"
             color="secondary"
             icon="public"
             name="Backgrounds"
             @clicked="$router.push('compendium/backgrounds')"
           />
-          <cc-major-btn
-            :cols="$vuetify.breakpoint.lgAndUp ? '3' : '6'"
+          <compendium-page-button
+            lg="3"
+            md="6"
+            sm="12"
             color="secondary"
             icon="cci-accuracy"
             name="Skill Triggers"
             @clicked="$router.push('compendium/skills')"
           />
-          <cc-major-btn
-            :cols="$vuetify.breakpoint.lgAndUp ? '3' : '6'"
+          <compendium-page-button
+            lg="3"
+            md="6"
+            sm="12"
             color="secondary"
             icon="cci-rank-3"
             name="Talents"
             @clicked="$router.push('compendium/talents')"
           />
-          <cc-major-btn
-            :cols="$vuetify.breakpoint.lgAndUp ? '4' : '6'"
+          <compendium-page-button
+            lg="4"
+            md="6"
+            sm="12"
             color="primary"
             icon="cci-role-striker"
             name="NPC Classes"
             @clicked="$router.push('compendium/npc_classes')"
           />
-          <cc-major-btn
-            :cols="$vuetify.breakpoint.lgAndUp ? '4' : '6'"
+          <compendium-page-button
+            lg="4"
+            md="6"
+            sm="12"
             color="primary"
             icon="cci-role-striker"
             name="NPC Features"
             @clicked="$router.push('compendium/npc_features')"
           />
-          <cc-major-btn
-            :cols="$vuetify.breakpoint.lgAndUp ? '4' : '6'"
+          <compendium-page-button
+            lg="4"
+            md="6"
+            sm="12"
             color="primary"
             icon="cci-role-striker"
             name="NPC Templates"
             @clicked="$router.push('compendium/npc_templates')"
           />
-          <cc-major-btn
-            :cols="$vuetify.breakpoint.lgAndUp ? '4' : '6'"
+          <compendium-page-button
+            lg="4"
+            md="6"
+            sm="12"
             icon="mdi-gauge"
             name="Statuses & Conditions"
             @clicked="$router.push('compendium/statuses')"
           />
-          <cc-major-btn
-            :cols="$vuetify.breakpoint.lgAndUp ? '4' : '6'"
+          <compendium-page-button
+            lg="4"
+            md="6"
+            sm="12"
             icon="cci-barrage"
             name="Reserves"
             @clicked="$router.push('compendium/reserves')"
           />
-          <cc-major-btn
-            :cols="$vuetify.breakpoint.lgAndUp ? '4' : '6'"
+          <compendium-page-button
+            lg="4"
+            md="6"
+            sm="12"
             icon="mdi-map"
             name="Action Economy"
             @clicked="$router.push('compendium/actions')"
           />
-          <cc-major-btn
-            :cols="$vuetify.breakpoint.lgAndUp ? '4' : '6'"
+          <compendium-page-button
+            lg="4"
+            md="6"
+            sm="12"
             icon="mdi-tag"
             name="Equipment Tags"
             @clicked="$router.push('compendium/tags')"
           />
-          <cc-major-btn
-            :cols="$vuetify.breakpoint.lgAndUp ? '4' : '6'"
+          <compendium-page-button
+            lg="4"
+            md="6"
+            sm="12"
             icon="mdi-map"
             name="Reference"
             @clicked="$router.push('compendium/reference')"
           />
-          <cc-major-btn
-            :cols="$vuetify.breakpoint.lgAndUp ? '4' : '6'"
+          <compendium-page-button
+            lg="4"
+            md="6"
+            sm="12"
             icon="mdi-library-books"
             name="Glossary"
             @clicked="$router.push('compendium/glossary')"
@@ -145,9 +185,10 @@
 <script lang="ts">
 import Vue from 'vue'
 import SearchBar from '../SearchBar.vue'
+import CompendiumPageButton from '../components/CompendiumPageButton.vue'
 
 export default Vue.extend({
   name: 'compendium-home',
-  components: { SearchBar },
+  components: { SearchBar, CompendiumPageButton },
 })
 </script>
