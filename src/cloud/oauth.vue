76ad4a1f4df9ba1699637886a4b4a6f6e95dd1b8
Johnny <jarena3@users.noreply.github.com>
2021-01-14 14:57:54 -0600

    again

--- src/cloud/oauth.vue
+++ src/cloud/oauth.vue
@@ -4,7 +4,7 @@
 
 <script lang="ts">
 import Vue from 'vue'
-import Patreon from './patreon'
+import { handleOAuthRedirectRequest } from './patreon'
 import axios from 'axios'
 import { CloudStore } from '@/store'
 import { getModule } from 'vuex-module-decorators'
@@ -14,25 +14,22 @@ export default Vue.extend({
   async created() {
     console.log(this.$route)
     const cloudstore = getModule(CloudStore, this.$store)
-    // ?code=k0eClWmn1nXgaK3Z52Noo4KOsPGdsp&state=chill
     const { code, state } = this.$route.query
     if (!code || !state) {
       this.$router.push('/')
       return
     }
-    const token = await Patreon.getUserToken(code, state).then(res => res.data)
-    cloudstore.setPatreonToken(token)
-    console.log(token)
-    const data = await Patreon.getUserCampaigns(token.access_token).then(res => res.data)
-    console.log(data)
+    handleOAuthRedirectRequest(code, { end: fn => console.log(fn) })
+    // const token = await Patreon.getUserToken(code, state).then(res => res.data)
+    // cloudstore.setPatreonToken(token)
+    // console.log(token)
+    // const data = await Patreon.getUserCampaigns(token.access_token).then(res => res.data)
+    // console.log(data)
 
     //cuse patreon API here to check against if they subscribe to patreon, based on this set true or false
 
     // if (this.code && this.state) cloudstore.IsPatron = true
     this.$router.push('/')
-  },
-  mounted() {
-    console.log(this.$route.query)
   },
 })
 </script>
