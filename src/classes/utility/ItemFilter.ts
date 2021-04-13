46fbfbd47ef63ad17fbb62ac6104f6236bfabd39
John Arena <john.arena.3@gmail.com>
2021-04-13 12:13:48 -0400

    fix(Compendium): item filter now includes weapon profile tags
    Closes #1331

--- src/classes/utility/ItemFilter.ts
+++ src/classes/utility/ItemFilter.ts
@@ -1,4 +1,4 @@
-import { MechEquipment, CompendiumItem } from '@/class'
+import { MechEquipment, CompendiumItem, MechWeapon } from '@/class'
 
 class ItemFilter {
   public static Filter(items: CompendiumItem[], filter: any): CompendiumItem[] {
@@ -6,8 +6,10 @@ class ItemFilter {
       if (p === 'LcpName') {
         items = items.filter(i => filter[p][0].includes(i.LcpName))
       } else if (p === 'Tags') {
-        items = items.filter((e: MechEquipment) =>
-          e.Tags.map(t => t.ID).some(x => filter.Tags.includes(x))
+        items = items.filter((e: MechEquipment) => {
+          if (e instanceof MechWeapon && e.ProfileTags?.map(t => t.ID).some(x => filter.Tags.includes(x))) return true
+          return e.Tags.map(t => t.ID).some(x => filter.Tags.includes(x))
+        }
         )
       } else if (p === 'SP_greater') {
         items = items.filter((x: MechEquipment) => x.SP > filter[p])
