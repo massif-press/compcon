05c70d198c3a83e6a36812efd69a121b0b535f77
John Arena <john.arena.3@gmail.com>
2021-04-21 12:39:09 -0400

    fix(Pilot Sheet): fix some new talent ui math
    Closes #1397

--- src/ui/components/Talent/components/_TalentSmall.vue
+++ src/ui/components/Talent/components/_TalentSmall.vue
@@ -82,7 +82,7 @@
                   v-if="n === parseInt(rank) + 1"
                   small
                   color="secondary"
-                  :disabled="canAdd"
+                  :disabled="!canAdd"
                   @click="$emit('add')"
                 >
                   <v-icon left>mdi-lock-open</v-icon>
