74dbd8ab156e87af2bc6731092ae009cfff9646a
Johnny <jarena3@users.noreply.github.com>
2021-06-04 14:59:53 -0500

    fix(Active Mode): correct stress repair interface
    Closes #1421

--- src/classes/mech/ActiveState.ts
+++ src/classes/mech/ActiveState.ts
@@ -357,7 +357,9 @@ class ActiveState {
   }
 
   RepairStress(): void {
-    this._mech.CurrentStress = this._mech.MaxStress
+    this._mech.CurrentStress += 1
+    if (this._mech.CurrentStress > this._mech.MaxStress)
+      this._mech.CurrentStress = this._mech.MaxStress
     const cheap = this._mech.Bonuses.some(x => x.ID === 'cheap_stress')
     this._mech.CurrentRepairs -= cheap ? 1 : 2
   }
@@ -471,15 +473,15 @@ class ActiveState {
       .getDate()
       .toString()
       .padStart(2, '0')}//${d
-        .getHours()
-        .toString()
-        .padStart(2, '0')}:${d
-          .getMinutes()
-          .toString()
-          .padStart(2, '0')}:${d
-            .getMilliseconds()
-            .toString()
-            .padStart(2, '0')}`
+      .getHours()
+      .toString()
+      .padStart(2, '0')}:${d
+      .getMinutes()
+      .toString()
+      .padStart(2, '0')}:${d
+      .getMilliseconds()
+      .toString()
+      .padStart(2, '0')}`
   }
 
   public SetLog(entry: { id: string; event: string; detail: string }, action?: Action) {
@@ -578,8 +580,9 @@ class ActiveState {
     this.SetLog({
       id: `set_move`,
       event: 'MOVE',
-      detail: `${val > 0 ? `FRAME/COMMIT.TAC: ${val} SPACES` : `FRAME/RESCIND.TAC: ${Math.abs(val)} SPACES`
-        }`,
+      detail: `${
+        val > 0 ? `FRAME/COMMIT.TAC: ${val} SPACES` : `FRAME/RESCIND.TAC: ${Math.abs(val)} SPACES`
+      }`,
     })
   }
 
@@ -601,8 +604,9 @@ class ActiveState {
     this.SetLog({
       id: `set_status`,
       event: 'STATUS',
-      detail: `${added ? '' : '!ALERT! '}FRAME/STATUS.${sstr.toUpperCase()} ++ALARM.${removed ? 'OFF' : 'ON'
-        }++`,
+      detail: `${added ? '' : '!ALERT! '}FRAME/STATUS.${sstr.toUpperCase()} ++ALARM.${
+        removed ? 'OFF' : 'ON'
+      }++`,
     })
   }
 
@@ -632,11 +636,13 @@ class ActiveState {
     this.SetLog({
       id: `set_burn`,
       event: 'BURN',
-      detail: `${val > 0
-        ? `!ALERT! FRAME/DMG.ONGOING: ${val} ++ALARM.ON++`
-        : `FRAME/DMG.MITIGATE: ${Math.abs(val)} ${this._mech.Burn > 0 ? '++ALARM.ON++' : '++ALARM.OFF++'
-        }`
-        }`,
+      detail: `${
+        val > 0
+          ? `!ALERT! FRAME/DMG.ONGOING: ${val} ++ALARM.ON++`
+          : `FRAME/DMG.MITIGATE: ${Math.abs(val)} ${
+              this._mech.Burn > 0 ? '++ALARM.ON++' : '++ALARM.OFF++'
+            }`
+      }`,
     })
   }
 
@@ -757,8 +763,9 @@ class ActiveState {
     this.SetLog({
       id: `set_rep`,
       event: 'REPAIR CAPACITY',
-      detail: `${val < 0 ? `FRAME/COMMIT.REPAIR: ${val}` : `FRAME/RECOVER.REPAIR: ${Math.abs(val)}`
-        }`,
+      detail: `${
+        val < 0 ? `FRAME/COMMIT.REPAIR: ${val}` : `FRAME/RECOVER.REPAIR: ${Math.abs(val)}`
+      }`,
     })
   }
 
@@ -768,8 +775,9 @@ class ActiveState {
     this.SetLog({
       id: `set_core`,
       event: 'CORE POWER',
-      detail: `${val > 0 ? `FRAME/CORE:: CAPACITY RESTORED` : `!ALERT! FRAME CORE ACTIVATION !ALERT!`
-        }`,
+      detail: `${
+        val > 0 ? `FRAME/CORE:: CAPACITY RESTORED` : `!ALERT! FRAME CORE ACTIVATION !ALERT!`
+      }`,
     })
   }
 
@@ -779,10 +787,11 @@ class ActiveState {
     this.SetLog({
       id: `set_oc`,
       event: 'OVERCHARGE',
-      detail: `${inc
-        ? `!WARN! FRAME/REACTOR.SYS::POWER REROUTE CONFIRM ++HEAT.ALARM.ON++`
-        : `FRAME/REACTOR.SYS::CHARGE PROTOCOL RECOVERY`
-        }`,
+      detail: `${
+        inc
+          ? `!WARN! FRAME/REACTOR.SYS::POWER REROUTE CONFIRM ++HEAT.ALARM.ON++`
+          : `FRAME/REACTOR.SYS::CHARGE PROTOCOL RECOVERY`
+      }`,
     })
   }
 
@@ -929,8 +938,9 @@ class ActiveState {
     this.SetLog({
       id: action,
       event: weapon.toUpperCase(),
-      detail: `${action.toUpperCase()}//${weapon.toUpperCase()}::${damage} DMG ${kill ? '++KILL CONFIRM++' : ''
-        }`,
+      detail: `${action.toUpperCase()}//${weapon.toUpperCase()}::${damage} DMG ${
+        kill ? '++KILL CONFIRM++' : ''
+      }`,
     })
   }
 
