e4e0ce7c4a35c842c6a089ddd7693f91fd59e96e
Timothy Myers <temyers240@gmail.com>
2021-08-09 15:00:13 -0500

    feat(Homebrew Content): restrict display of source LCP to none-core book

--- src/classes/Background.ts
+++ src/classes/Background.ts
@@ -9,12 +9,14 @@ class Background {
     public readonly Name: string
     public readonly Description: string
     public readonly LcpName: string
+    public readonly InLcp: boolean
 
     public constructor(data: IBackgroundData, packName?: string) {
         this.ID = data.id
         this.Name = data.name
         this.Description = data.description
         this.LcpName = packName || 'LANCER Core Book'
+        this.InLcp = packName ? true : false
     }
 
 }
