b20ffaa31aa98a424c756ccd27426a2fa7eb3eff
Timothy Myers <temyers240@gmail.com>
2021-08-16 10:42:24 -0500

    fix(Active Mode): sync data at the end of each turn
    Closes #1622

--- src/features/pilot_management/ActiveSheet/layout/footers/CombatFooter.vue
+++ src/features/pilot_management/ActiveSheet/layout/footers/CombatFooter.vue
@@ -307,6 +307,7 @@ export default vueMixins(activePilot).extend({
     },
     endTurn(burn) {
       this.state.EndTurn(burn?.hp || 0, burn?.str || 0)
+      this.$store.dispatch('cloudSync', { callback: null, condition: 'turnEnd' })
       this.turnConfirm = false
     },
     undoEndTurn() {
