a5fcbd706c35761992cbd70823f23f6cbe1245ec
John Arena <john.arena.3@gmail.com>
2022-02-14 10:06:30 -0500

    remove redundant registration

--- src/registerServiceWorker.ts
+++ src/registerServiceWorker.ts
@@ -30,18 +30,4 @@ if (process.env.VUE_APP_NODE_ENV === 'production') {
       console.error('Error during service worker registration:', error)
     }
   })
-
-
-  if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-      navigator.serviceWorker
-        .register('/sw.js')
-        .then(registration => {
-          console.log('SW registered: ', registration)
-        })
-        .catch(registrationError => {
-          console.log('SW registration failed: ', registrationError)
-        })
-    })
-  }
 }
