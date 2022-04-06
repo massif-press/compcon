ea0fba60e318dea21749a96291e143a1796c6630
karidyas <jonathancaridia@gmail.com>
2022-04-06 20:10:22 +1200

    refactor(UX): merge Tactical Profile and Hangar onto one tab

--- src/features/pilot_management/PilotSheet/sections/tactical/index.vue
+++ src/features/pilot_management/PilotSheet/sections/tactical/index.vue
@@ -1,10 +1,59 @@
 <template>
-  <div>
+  <div class="mt-6">
     <license-block :pilot="pilot" class="mb-4" />
     <special-block :pilot="pilot" class="mb-4" />
     <mech-skills-block :pilot="pilot" class="my-4" />
     <core-bonus-block :pilot="pilot" class="my-4" />
     <talent-block :pilot="pilot" class="mt-4" />
+
+    <cc-title large color="pilot" class="ml-n10 pl-3 mb-2">Hangar&emsp;</cc-title>
+    <v-btn-toggle id="viewtoggle" :value="profile.GetView('hangar')" mandatory>
+      <v-btn small icon value="cards" @click="profile.SetView('hangar', 'cards')">
+        <v-icon color="accent">mdi-view-grid</v-icon>
+      </v-btn>
+      <v-btn small icon value="list" @click="profile.SetView('hangar', 'list')">
+        <v-icon color="accent">mdi-view-list</v-icon>
+      </v-btn>
+      <v-btn small icon value="table" @click="profile.SetView('hangar', 'table')">
+        <v-icon color="accent">mdi-format-align-justify</v-icon>
+      </v-btn>
+    </v-btn-toggle>
+    <v-container v-if="profile.GetView('hangar') === 'cards'" fluid>
+      <v-row justify="center">
+        <mech-card
+          v-for="m in pilot.Mechs"
+          :key="`${m.ID}_mc`"
+          :mech="m"
+          @delete="pilot.RemoveMech($event)"
+          @copy="pilot.CloneMech($event)"
+          @go="toMechSheet($event)"
+        />
+      </v-row>
+    </v-container>
+    <v-container v-else-if="profile.GetView('hangar') === 'list'" class="mt-2 px-0" fluid>
+      <mech-list-item
+        v-for="m in pilot.Mechs"
+        :key="`${m.ID}_mc`"
+        :mech="m"
+        @delete="pilot.RemoveMech($event)"
+        @copy="pilot.CloneMech($event)"
+        @go="toMechSheet($event)"
+      />
+    </v-container>
+    <v-container v-else class="px-0">
+      <mech-table :mechs="pilot.Mechs" @go="toMechSheet($event)" />
+    </v-container>
+    <v-row justify="center">
+      <v-col cols="auto">
+        <cc-btn x-large class="ml-auto mr-auto" @click="$refs.dialog.show()">
+          <v-icon left large>cci-accuracy</v-icon>
+          &emsp;Add New Mech
+        </cc-btn>
+      </v-col>
+    </v-row>
+    <cc-solo-dialog ref="dialog" icon="cci-frame" no-confirm title="Add New Mech" fullscreen>
+      <new-mech-menu :pilot="pilot" @close="$refs.dialog.hide()" />
+    </cc-solo-dialog>
   </div>
 </template>
 
@@ -15,15 +64,42 @@ import LicenseBlock from './components/LicenseBlock.vue'
 import SpecialBlock from './components/SpecialBlock.vue'
 import CoreBonusBlock from './components/CoreBonusBlock.vue'
 import TalentBlock from './components/TalentBlock.vue'
+import MechCard from './components/MechCard.vue'
+import MechListItem from './components/MechListItem.vue'
+import MechTable from './components/MechTable.vue'
+import NewMechMenu from './components/NewMechMenu.vue'
+import { getModule } from 'vuex-module-decorators'
+import { UserStore, PilotManagementStore } from '@/store'
+import { Pilot } from '@/class'
+import { UserProfile } from '@/user'
 
 export default Vue.extend({
   name: 'tactical-view',
-  components: { MechSkillsBlock, SpecialBlock, LicenseBlock, CoreBonusBlock, TalentBlock },
+  components: { MechSkillsBlock, SpecialBlock, LicenseBlock, CoreBonusBlock, TalentBlock, MechCard, MechListItem, MechTable, NewMechMenu },
   props: {
     pilot: {
       type: Object,
       required: true,
     },
   },
+  computed: {
+    profile(): UserProfile {
+      const store = getModule(UserStore, this.$store)
+      return store.UserProfile
+    },
+  },
+  methods: {
+    toMechSheet(mech) {
+      const store = getModule(PilotManagementStore, this.$store)
+      store.setLoadedMech(mech.ID)
+      this.$router.push(`../mech/${mech.ID}`)
+    },
+  },
 })
 </script>
+
+<style scoped>
+#viewtoggle {
+  z-index: 4;
+}
+</style>
