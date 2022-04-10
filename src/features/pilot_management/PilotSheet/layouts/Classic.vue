bbf544bace302768e5ec1f2e447e4b2a5ecd1714
karidyas <jonathancaridia@gmail.com>
2022-04-10 20:40:39 +1200

    refactor(UX): Edited pilot views and nav to reflect page merging
    Removed sections that aren't used any more due to being merged into

--- src/features/pilot_management/PilotSheet/layouts/Classic.vue
+++ src/features/pilot_management/PilotSheet/layouts/Classic.vue
@@ -1,22 +1,18 @@
 <template>
   <div>
     <info-view id="section-0" :pilot="pilot" />
-    <narrative-view id="section-1" :pilot="pilot" />
-    <tactical-view id="section-2" :pilot="pilot" />
-    <mech-hangar-view id="section-3" :pilot="pilot" />
+    <tactical-view id="section-1" :pilot="pilot" />
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
-import NarrativeView from '../sections/narrative/index.vue'
 import InfoView from '../sections/info/index.vue'
 import TacticalView from '../sections/tactical/index.vue'
-import MechHangarView from '../sections/hangar/index.vue'
 
 export default Vue.extend({
   name: 'classic',
-  components: { NarrativeView, InfoView, TacticalView, MechHangarView },
+  components: { InfoView, TacticalView },
   props: {
     pilot: {
       type: Object,
