5399a0dd83038f058737faf83964c7c96db2dd8d
Johnny <jarena3@users.noreply.github.com>
2022-05-10 09:59:52 -0500

    sw changes

--- src/registerServiceWorker.ts
+++ src/registerServiceWorker.ts
@@ -1,7 +1,17 @@
 /* eslint-disable no-console */
 
 import { register } from 'register-service-worker'
-const workers = [`${process.env.BASE_URL}service-worker.js`, `${process.env.BASE_URL}sw.js`]
+
+// find and unregister old serviceworker
+const registrations = await navigator.serviceWorker.getRegistrations()
+const matchingRegistrations = registrations.filter(registration => {
+  return registration.active.scriptURL.includes('sw.js')
+})
+
+for (const registration of matchingRegistrations) {
+  await registration.unregister()
+  console.log('Unregistered old sw:', registration)
+}
 
 if (navigator && navigator.serviceWorker) {
   navigator.serviceWorker
@@ -22,33 +32,30 @@ if (navigator && navigator.serviceWorker) {
 }
 
 if (process.env.VUE_APP_NODE_ENV === 'production') {
-  for (const w of workers) {
-    console.log(w)
-    register(w, {
-      ready() {
-        console.log(
-          'App is being served from cache by a service worker.\n' +
-            'For more details, visit https://goo.gl/AFskqB'
-        )
-      },
-      registered() {
-        console.info('Service worker has been registered.')
-      },
-      cached() {
-        console.info('Content has been cached for offline use.')
-      },
-      updatefound() {
-        console.info('New content is downloading.')
-      },
-      updated() {
-        console.info('New content is available; please refresh.')
-      },
-      offline() {
-        console.info('No internet connection found. App is running in offline mode.')
-      },
-      error(error) {
-        console.error('Error during service worker registration:', error)
-      },
-    })
-  }
+  register(`${process.env.BASE_URL}service-worker.js`, {
+    ready() {
+      console.log(
+        'App is being served from cache by a service worker.\n' +
+          'For more details, visit https://goo.gl/AFskqB'
+      )
+    },
+    registered() {
+      console.info('Service worker has been registered.')
+    },
+    cached() {
+      console.info('Content has been cached for offline use.')
+    },
+    updatefound() {
+      console.info('New content is downloading.')
+    },
+    updated() {
+      console.info('New content is available; please refresh.')
+    },
+    offline() {
+      console.info('No internet connection found. App is running in offline mode.')
+    },
+    error(error) {
+      console.error('Error during service worker registration:', error)
+    },
+  })
 }
