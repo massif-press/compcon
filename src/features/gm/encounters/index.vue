c35d39537c378cbd201714241a2099aa9b2908ff
John Arena <john.arena.3@gmail.com>
2022-05-20 13:48:16 -0500

    begin storage api work

--- src/features/gm/encounters/index.vue
+++ src/features/gm/encounters/index.vue
@@ -2,124 +2,19 @@
   <v-container>
     <div class="my-2">
       <div class="heading h3">Active Encounters</div>
-      <v-simple-table class="mb-2">
-        <thead>
-          <th>Encounter</th>
-          <th>Campaign</th>
-          <th>SITREP</th>
-          <th>NPCs</th>
-          <th>Pilots</th>
-          <th>Turn</th>
-          <th></th>
-        </thead>
-        <tbody>
-          <tr class="text-center">
-            <td class="text-left">Example Encounter</td>
-            <td>My Campaign</td>
-            <td>GAUNTLET</td>
-            <td>
-              <v-menu bottom offset-y offset-x open-on-hover>
-                <template v-slot:activator="{ on }">
-                  <v-btn
-                    text
-                    color="primary"
-                    style="min-width: 0; min-height: 0"
-                    class="px-1 py-1"
-                    x-large
-                    @click.stop
-                    v-on="on"
-                  >
-                    <b>5</b>
-                  </v-btn>
-                </template>
-                <v-list dense>
-                  <v-list-item>NPC Name #1</v-list-item>
-                  <v-list-item>NPC Name #2</v-list-item>
-                  <v-list-item>NPC Name #3</v-list-item>
-                  <v-list-item>NPC Name #4</v-list-item>
-                  <v-list-item>NPC Name #5</v-list-item>
-                </v-list>
-              </v-menu>
-              (
-              <v-menu bottom offset-y offset-x open-on-hover>
-                <template v-slot:activator="{ on }">
-                  <v-btn
-                    text
-                    color="primary"
-                    style="min-width: 0; min-height: 0"
-                    class="px-1 py-1"
-                    x-large
-                    @click.stop
-                    v-on="on"
-                  >
-                    <b>2</b>
-                  </v-btn>
-                </template>
-                <v-list dense>
-                  <v-list-item>Dead NPC #1</v-list-item>
-                  <v-list-item>Dead NPC #2</v-list-item>
-                </v-list>
-              </v-menu>
-              <v-icon small>mdi-skull</v-icon>
-              )
-            </td>
-            <td>
-              <v-menu bottom offset-y offset-x open-on-hover>
-                <template v-slot:activator="{ on }">
-                  <v-btn
-                    text
-                    color="primary"
-                    style="min-width: 0; min-height: 0"
-                    class="px-1 py-1"
-                    x-large
-                    @click.stop
-                    v-on="on"
-                  >
-                    <b>3</b>
-                  </v-btn>
-                </template>
-                <v-list dense>
-                  <v-list-item>Pilot Callsign #1</v-list-item>
-                  <v-list-item>Pilot Callsign #2</v-list-item>
-                  <v-list-item>Pilot Callsign #3</v-list-item>
-                </v-list>
-              </v-menu>
-            </td>
-            <td>4</td>
-            <v-btn class="mb-n4" color="accent">Open</v-btn>
-          </tr>
-        </tbody>
-      </v-simple-table>
+      <encounter-table active />
 
       <div class="heading h3">Encounter Library</div>
-      <v-simple-table class="mb-2">
-        <thead>
-          <th>Encounter</th>
-          <th>Campaign</th>
-          <th>SITREP</th>
-          <th>NPCs</th>
-          <th></th>
-          <th></th>
-        </thead>
-        <tbody>
-          <tr class="text-center">
-            <td class="text-left">Example Encounter</td>
-            <td>My Campaign</td>
-            <td>GAUNTLET</td>
-            <td>7</td>
-            <td><v-btn color="accent">Edit</v-btn></td>
-            <td><v-btn color="accent">Run</v-btn></td>
-          </tr>
-          <tr class="text-center">
-            <td class="text-left">WIP Encounter</td>
-            <td>--</td>
-            <td>ELIMINATION</td>
-            <td>8</td>
-            <td><v-btn color="accent">Edit</v-btn></td>
-            <td><v-btn color="accent">Run</v-btn></td>
-          </tr>
-        </tbody>
-      </v-simple-table>
+      <v-tabs>
+        <v-tab>No Campaign</v-tab>
+        <v-tab>My Campaign A</v-tab>
+        <v-tab-item>
+          <encounter-table />
+        </v-tab-item>
+        <v-tab-item>
+          <encounter-table />
+        </v-tab-item>
+      </v-tabs>
     </div>
     <v-row justify="center" align="center">
       <v-col cols="8">
@@ -140,7 +35,10 @@
 
 <script lang="ts">
 import Vue from 'vue'
+import EncounterTable from './_components/EncounterTable.vue'
+
 export default Vue.extend({
   name: 'encounter-landing',
+  components: { EncounterTable },
 })
 </script>
