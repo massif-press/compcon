d49e263a970d8752a4701d3f208c41497104edd6
Toren Darby <darby@science.ubc.ca>
2022-05-26 15:38:43 -0700

    fix(accessibility): Make CCRating cells not tabbable, since they're read only.

--- src/ui/components/CCRating.vue
+++ src/ui/components/CCRating.vue
@@ -1,17 +1,6 @@
 <template>
-  <v-rating
-    v-model="model"
-    hover
-    x-large
-    :dense="dense"
-    :length="max"
-    readonly
-    :color="color"
-    class="d-inline"
-    empty-icon="mdi-hexagon-outline"
-    full-icon="mdi-hexagon-slice-6"
-    background-color="panel"
-  />
+  <v-rating v-model="model" hover x-large :dense="dense" :length="max" readonly :color="color" class="d-inline"
+    empty-icon="mdi-hexagon-outline" full-icon="mdi-hexagon-slice-6" background-color="panel" />
 </template>
 
 <script lang="ts">
@@ -28,6 +17,16 @@ export default class CCRating extends Vue {
   readonly dense?: boolean
   @Prop({ type: String, required: false, default: 'accent' })
   readonly color: string
+
+  mounted() {
+    // v-rating made the cells of the rating bar tabble even when read only. This overwrites them to be non-tabable.
+    // If ever more elements are added to this component, check that this isn't making them untabable.
+    this.$children.forEach(ratingBar => {
+      ratingBar.$children.forEach(cell => {
+        cell.$el.setAttribute('tabindex', '-1')
+      })
+    })
+  }
 }
 
 export { CCRating as CCRanking }
