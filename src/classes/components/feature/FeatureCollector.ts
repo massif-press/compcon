583e918b40ac8aeb482f7f6baa8251397bf93c13
John Arena <john.arena.3@gmail.com>
2022-06-28 18:04:29 -0400

    fix #2005, #2037

--- src/classes/components/feature/FeatureCollector.ts
+++ src/classes/components/feature/FeatureCollector.ts
@@ -11,7 +11,12 @@ abstract class FeatureCollector {
     element[property].forEach(candidate => {
       if (candidate['Destroyed'] !== undefined && (candidate as any).Destroyed) return
       if (candidate['IsCascading'] !== undefined && (candidate as any).IsCascading) return
-      if (candidate['Used'] !== undefined && (candidate as any).Used) return
+      if (
+        collection.toLowerCase() !== 'action' &&
+        candidate['Used'] !== undefined &&
+        (candidate as any).Used
+      )
+        return
       out.push(candidate as T)
     })
     return out
