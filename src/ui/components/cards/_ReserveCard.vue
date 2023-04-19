afd087be2683bb13c1aa1894aed9dede242052a1
John A <john.arena.3@gmail.com>
2023-04-19 13:11:28 -0500

    resolve #2198

--- src/ui/components/cards/_ReserveCard.vue
+++ src/ui/components/cards/_ReserveCard.vue
@@ -1,19 +1,19 @@
 <template>
-    <cc-titled-panel :title="item.Name" :icon="item.Icon" :color="item.Color">
-    <v-card-text class="text-xs-left mt-0 pt-0" style="height: 115px">
-        <div class="overline subtle--text mt-n2 mb-n1">{{ item.Type }}</div>
-        <p v-html-safe="item.Description" class="flavor-text mb-0" />
+  <cc-titled-panel :title="item.Name" :icon="item.Icon" :color="item.Color">
+    <v-card-text class="text-xs-left mt-0 pt-0" style="min-height: 115px">
+      <div class="overline subtle--text mt-n2 mb-n1">{{ item.Type }}</div>
+      <p v-html-safe="item.Description" class="flavor-text mb-0" />
     </v-card-text>
-    </cc-titled-panel>
+  </cc-titled-panel>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 
 export default Vue.extend({
-    name: 'cc-reserve-card',
-    props: {
-        item: { type: Object, required: true}
-    }
+  name: 'cc-reserve-card',
+  props: {
+    item: { type: Object, required: true },
+  },
 })
 </script>
