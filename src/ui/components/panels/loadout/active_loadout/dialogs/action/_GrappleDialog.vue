c7db15b2f61ac9315f14486ffd7eaef7acf4f7d9
John A <john.arena.3@gmail.com>
2023-05-09 16:04:31 -0500

    fix build error

--- src/ui/components/panels/loadout/active_loadout/dialogs/action/_GrappleDialog.vue
+++ src/ui/components/panels/loadout/active_loadout/dialogs/action/_GrappleDialog.vue
@@ -76,7 +76,6 @@
               <v-text-field
                 v-model="accuracy"
                 type="number"
-                append-icon="mdi-plus-circle-outline"
                 append-icon="cc:accuracy"
                 prepend-icon="mdi-minus-circle-outline"
                 style="width: 115px"
@@ -98,7 +97,6 @@
               <v-text-field
                 v-model="difficulty"
                 type="number"
-                append-icon="mdi-plus-circle-outline"
                 append-icon="cc:difficulty"
                 prepend-icon="mdi-minus-circle-outline"
                 style="width: 115px"
