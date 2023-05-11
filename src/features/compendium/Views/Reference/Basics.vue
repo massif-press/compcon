e715ddc29276e5c1590e0f8258771ce0c786e006
John A <john.arena.3@gmail.com>
2023-05-11 16:47:10 -0500

    early lang experiments

--- src/features/compendium/Views/Reference/Basics.vue
+++ src/features/compendium/Views/Reference/Basics.vue
@@ -1,4 +1,7 @@
 <template>
+  <div style="position: fixed; top: 60px; right: 30px">
+    <switch-lang-temp />
+  </div>
   <h1 class="heading h1 ml-5">LANCER BASICS</h1>
   <srd-view :content="basics" :pre-scroll="preScroll" />
 </template>
@@ -6,12 +9,14 @@
 <script lang="ts">
 import basics from '@/assets/srd/lib/basics.json';
 import SrdView from './_components/SrdView.vue';
+import SwitchLangTemp from './_components/SwitchLangTEMP.vue';
 
 export default {
   name: 'basics',
   inheritAttrs: false,
   components: {
     SrdView,
+    SwitchLangTemp,
   },
   props: {
     preScroll: {
---
+++ src/features/compendium/Views/Reference/_components/SwitchLangTEMP.vue
@@ -0,0 +1,88 @@
+<template>
+  <div>
+    <v-btn variant="outlined" size="small" class="mx-3" color="secondary"
+      ><v-icon start icon="mdi-translate" />{{ lang }}
+      <v-menu activator="parent" width="500px">
+        <v-card>
+          <v-card-text>
+            <v-alert
+              icon="mdi-alert"
+              color="error"
+              title="EXPERIMENTAL PREVIEW"
+              variant="outlined"
+              density="compact"
+            >
+              This is an experimental preview of the language selection feature. Non-English
+              translation is currently machine-generated and may be inaccurate.</v-alert
+            >
+            <v-list>
+              <v-list-item
+                v-for="lang in languages"
+                :key="lang.title"
+                :title="lang.title"
+                @click="setLang(lang.value)"
+              />
+            </v-list>
+          </v-card-text>
+        </v-card>
+      </v-menu>
+    </v-btn>
+  </div>
+</template>
+
+<script lang="ts">
+import { NavStore } from '@/stores';
+export default {
+  name: 'temp-language-selector',
+  data: () => ({
+    languages: [
+      {
+        title: 'English',
+        value: 'en',
+      },
+      {
+        title: 'Español',
+        value: 'es',
+      },
+      {
+        title: 'Deutsch',
+        value: 'de',
+      },
+      {
+        title: 'Français',
+        value: 'fr',
+      },
+      {
+        title: 'Italiano',
+        value: 'it',
+      },
+      {
+        title: 'Русский',
+        value: 'ru',
+      },
+      {
+        title: '日本語',
+        value: 'ja',
+      },
+      {
+        title: '한국어',
+        value: 'ko',
+      },
+      {
+        title: '中文',
+        value: 'zh',
+      },
+    ],
+  }),
+  computed: {
+    lang() {
+      return NavStore().Language;
+    },
+  },
+  methods: {
+    setLang(lang: string) {
+      NavStore().setLanguage(lang);
+    },
+  },
+};
+</script>
