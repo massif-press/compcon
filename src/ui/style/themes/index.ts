b9e1915f5c04707ccd3e1b81ed3c03e6e6636633
vialra <108127897+vialra@users.noreply.github.com>
2024-02-04 12:55:34 +0100

    correction ssc base ipsn

--- src/ui/style/themes/index.ts
+++ src/ui/style/themes/index.ts
@@ -2,5 +2,6 @@ import gms from './theme_gms'
 import horus from './theme_horus'
 import msmc from './theme_msmc'
 import ssc from './theme_ssc'
+import ipsn from './theme_ipsn'
 
-export default { gms, horus, ssc, msmc }
+export default { gms, horus, ssc, ipsn, msmc }
