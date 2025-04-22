cd8673e676229437f8228eb774ec97fa1643a545
John A <john.arena.3@gmail.com>
2025-04-21 22:29:01 -0500

    minor compendium item changes

--- src/ui/components/items/CCCoreBonusItem.vue
+++ src/ui/components/items/CCCoreBonusItem.vue
@@ -1,7 +1,12 @@
 <template>
-  <cc-panel :title="bonus.Name" :icon="bonus.Icon" :title-color="bonus.Color">
-    <p v-if="bonus.InLcp" v-html-safe="bonus.LcpName" class="text-stat heading h3" />
-    <p v-if="!terse" v-html-safe="bonus.Description" class="flavor-text mb-2" />
+  <cc-panel
+    :title="bonus.Name"
+    :icon="bonus.Icon"
+    :title-color="bonus.Color"
+    style="position: relative">
+    <div v-if="bonus.InLcp" style="position: absolute; top: 27px; right: 4px; z-index: 1">
+      <cc-lcp-info v-if="bonus.InLcp" :item="bonus" />
+    </div>
     <p v-html-safe="bonus.Effect" class="pb-1" />
 
     <div v-if="bonus.Actions.length">
