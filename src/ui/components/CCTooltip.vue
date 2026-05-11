e664fab3da63b46cba3043affcaedf697e4c0d81
J. Arena <john.arena.3@gmail.com>
2026-05-11 10:36:13 -0500

    fix cc-tooltip

--- src/ui/components/CCTooltip.vue
+++ src/ui/components/CCTooltip.vue
@@ -20,57 +20,6 @@
   </v-tooltip>
 </template>
 
-<<<<<<< Updated upstream
-<script lang="ts">
-import { Anchor } from 'vuetify';
-
-
-export default {
-  props: {
-    text: {
-      type: String,
-    },
-    icon: {
-      type: String,
-    },
-    start: {
-      type: Boolean,
-      default: false,
-    },
-    end: {
-      type: Boolean,
-      default: false,
-    },
-    size: {
-      type: [String, Number],
-      default: undefined,
-    },
-    location: {
-      type: String,
-      default: 'top',
-    },
-    color: {
-      type: String,
-      default: '',
-    },
-  },
-  computed: {
-    getIcon() {
-      return this.icon || 'mdi-information-slab-box-outline';
-    },
-    showTooltip() {
-      return this.text || this.$slots.default;
-    },
-    mobile(): boolean {
-      return 'ontouchstart' in window || navigator.maxTouchPoints > 0;
-    },
-    getLocation() {
-      if (this.mobile) return 'top' as Anchor;
-      return this.location as Anchor;
-    },
-  },
-};
-=======
 <script setup lang="ts">
 import { computed, useSlots } from 'vue'
 import type { Anchor } from 'vuetify'
@@ -98,5 +47,4 @@ const getIcon = computed(() => props.icon || 'mdi-information-slab-box-outline')
 const showTooltip = computed(() => props.text || slots.default)
 const mobile = computed(() => 'ontouchstart' in window || navigator.maxTouchPoints > 0)
 const getLocation = computed(() => (mobile.value ? 'top' : props.location) as Anchor)
->>>>>>> Stashed changes
 </script>
