4380fc4c9ed26c79b3005fc480b940bea1ae6a43
Luiz paulo de souza fontes junior <82241012+LPfontes@users.noreply.github.com>
2026-07-26 03:07:55 -0300

    Change: apply i18n to the Reserves

--- src/features/compendium/Views/Compendium/Reserves.vue
+++ src/features/compendium/Views/Compendium/Reserves.vue
@@ -1,9 +1,11 @@
 <template>
-  <cc-compendium-browser :items="reserves"
+  <cc-compendium-browser
+    :items="reserves"
     item-type="Reserve"
     :table-headers="headers"
     :options="options"
-    view-key="cb-reserves">
+    view-key="cb-reserves"
+  >
     <template #header>
       <div class="heading h3 text-center text-accent">{{ $t('common.reserves') }}</div>
     </template>
@@ -11,29 +13,29 @@
 </template>
 
 <script setup lang="ts">
-import { computed, ref } from 'vue'
-import { CompendiumStore } from '@/stores';
-import { orderBy } from 'lodash-es';
-import { useI18n } from 'vue-i18n'
-const { t } = useI18n()
+  import { computed, ref } from 'vue'
+  import { CompendiumStore } from '@/stores'
+  import { orderBy } from 'lodash-es'
+  import { useI18n } from 'vue-i18n'
+  const { t } = useI18n()
 
-const headers = ref([
-      { title: t('compendium.titles.contentPack'), key: 'LcpName' },
-      { title: 'Name', key: 'Name' },
-      { title: t('common.type'), key: 'Type' },
-    ])
-const options = ref({
-      views: ['list', 'table'],
-      initialView: 'list',
-      groups: ['lcp', 'type', 'none'],
-      initialGroup: 'type',
-      noSource: true,
-    })
+  const headers = ref([
+    { title: t('compendium.titles.contentPack'), key: 'LcpName' },
+    { title: t('common.name'), key: 'Name' },
+    { title: t('common.type'), key: 'Type' },
+  ])
+  const options = ref({
+    views: ['list', 'table'],
+    initialView: 'list',
+    groups: ['lcp', 'type', 'none'],
+    initialGroup: 'type',
+    noSource: true,
+  })
 
-const reserves = computed(() => {
-      return orderBy(
-        CompendiumStore().Reserves.filter((x) => !x.IsHidden),
-        'Name'
-      );
-    })
+  const reserves = computed(() => {
+    return orderBy(
+      CompendiumStore().Reserves.filter(x => !x.IsHidden),
+      'Name'
+    )
+  })
 </script>
